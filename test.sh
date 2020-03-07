f331ffeaa20b19115c2d79431c3ab6663c9d3723
devizer <devizer@users.github.com>
2020-03-07 20:48:50 +0200

    initial

---
+++ test.sh
@@ -0,0 +1,6 @@
+# work=$HOME/build/azure-pipeline-agent-in-docker; mkdir -p $(dirname $work); cd $(dirname $work); git clone https://github.com/devizer/azure-pipeline-agent-in-docker || true; cd azure-pipeline-agent-in-docker; git pull; bash test.sh
+
+docker image rm devizervlad/azpa
+cd armv7
+docker build -t devizervlad/azpa:1 .
+
