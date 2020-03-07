50ad019e570d2acf942fca1552a33d7adc177662
devizer <devizer@users.github.com>
2020-03-07 22:29:18 +0200

    updated

---
+++ armv7/install-apa.sh
@@ -0,0 +1,22 @@
+work=$HOME/pipeline-agent
+mkdir -p $work
+cd $work
+Say "azure pipeline agent path: [$(pwd)]"
+wget -O linux-agent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.165.0/vsts-agent-linux-arm-2.165.0.tar.gz
+tar xzf linux-agent.tar.gz
+# ./bin/Agent.Listener configure --unattended \
+./config.sh --unattended \
+  --url "${VSTS_URL}" \
+  --work "${VSTS_WORK:-_work}" \
+  --auth pat --token "$VSTS_PAT" --pool "${VSTS_POOL:-Default}" --replace & wait $!
+
+function _ignore_() {
+./bin/Agent.Listener configure --unattended \
+  --agent "${VSTS_AGENT:-$(hostname)}" \
+  --url "https://$VSTS_ACCOUNT.visualstudio.com" \
+  --auth PAT \
+  --token $(cat "$VSTS_TOKEN_FILE") \
+  --pool "${VSTS_POOL:-Default}" \
+  --work "${VSTS_WORK:-_work}" \
+  --replace & wait $!
+}
