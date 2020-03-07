a4b7b30a57a1de0c2575ea8c4d98e6bbc43b9a9e
devizer <devizer@users.github.com>
2020-03-07 23:35:58 +0200

    updated

--- armv7/install-apa.sh
+++ armv7/install-apa.sh
@@ -5,7 +5,7 @@ cd $work
 Say "azure pipeline agent path: [$(pwd)]"
 printenv | sort
 suffix=linux-arm
-if [[ "$(uname -m)" == "x86_64" ]]; suffix=linux-x64; fi 
+if [[ "$(uname -m)" == "x86_64" ]]; then suffix=linux-x64; fi
 wget --progress=bar:force:noscroll -O linux-agent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.165.0/vsts-agent-${suffix}-2.165.0.tar.gz
 tar xzf linux-agent.tar.gz
 # ./bin/Agent.Listener configure --unattended \
