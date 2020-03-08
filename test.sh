ee2a3849dc8610d16a071e26ea3185a2f1ffcf77
devizer <devizer@users.github.com>
2020-03-08 09:59:00 +0200

    updated

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # work=$HOME/build/azure-pipeline-agent-in-docker; mkdir -p $(dirname $work); cd $(dirname $work); git clone https://github.com/devizer/azure-pipeline-agent-in-docker || true; cd azure-pipeline-agent-in-docker; git pull; time bash test.sh
 
-docker image rm devizervlad/azpa
+docker image rm -f devizervlad/azpa
+# docker image rm -f $(docker image ls -aq)
 cd armv7
-time docker build --build-arg VSTS_URL --build-arg VSTS_POOL --build-arg VSTS_AGENT --build-arg VSTS_PAT --build-arg VSTS_WORK -t devizervlad/azpa:1 .
-docker run --hotname agent007 --rm -it devizervlad/azpa:1 
+time docker build --build-arg VSTS_URL --build-arg VSTS_POOL --build-arg VSTS_AGENT --build-arg VSTS_PAT --build-arg VSTS_WORK -t devizervlad/azpa:latest .
+docker run --hotname agent007 --rm -it devizervlad/azpa:latest 
 
