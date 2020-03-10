9a24d8c5c8f3c5ebab9c9b4cb1a828b2c722bfe0
devizer <devizer@users.github.com>
2020-03-10 17:29:01 +0200

    restart on fail

--- test.sh
+++ test.sh
@@ -5,5 +5,4 @@ docker image rm -f devizervlad/azpa:latest
 # docker image rm -f $(docker image ls -aq)
 cd armv7
 time docker build --build-arg VSTS_URL --build-arg VSTS_POOL --build-arg VSTS_AGENT --build-arg VSTS_PAT --build-arg VSTS_WORK -t devizervlad/azpa:latest .
-docker run --name agent007 --privileged --hostname agent007 --rm -it devizervlad/azpa:latest 
-
+docker run --restart on-failure --name agent007 --privileged --hostname agent007 --rm -it devizervlad/azpa:latest 
