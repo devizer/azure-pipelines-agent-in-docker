7c8b79f45711341647425cb75f43b73ab7a71e50
devizer <devizer@users.github.com>
2020-03-13 01:43:15 +0200

    test cross-platofrom docker image in azure pipelines again (pass VSTS)

---
+++ cross-platform/disable-ipv6.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo '
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+' | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+
