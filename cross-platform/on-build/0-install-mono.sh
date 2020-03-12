d06bf1dcfb3db9294eb4ca00a612dd150378d4c6
devizer <devizer@users.github.com>
2020-03-13 01:57:46 +0200

    disable ipv6 on host and containers

---
+++ cross-platform/on-build/0-install-mono.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+script=https://raw.githubusercontent.com/devizer/test-and-build/master/lab/install-MONO.sh
+(wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash; 
