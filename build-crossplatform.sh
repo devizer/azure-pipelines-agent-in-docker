3c239996113b8b08cd6474bc567b8d54948283f7
devizer <devizer@users.github.com>
2020-03-12 12:16:51 +0200

    try cross-platofrom docker image

--- build-crossplatform.sh
+++ build-crossplatform.sh
@@ -5,5 +5,4 @@ docker image rm -f devizervlad/azpa:latest
 # docker image rm -f $(docker image ls -aq)
 cd cross-platform
 time docker build -t devizervlad/crossplatform-azure-pipelines-agent:latest .
-time 
 # docker run --restart on-failure --name agent007 --privileged --hostname agent007 -it devizervlad/azpa:latest 
