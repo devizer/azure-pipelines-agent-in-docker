dc04403a51679bcea1bf828e6bff5fa2c0e09bed
devizer <devizer@users.github.com>
2020-03-12 21:12:04 +0200

    test cross-platofrom docker image in azure pipelines again

--- cross-platform/on-build/3-install-dotnet.sh
+++ cross-platform/on-build/3-install-dotnet.sh
@@ -3,6 +3,7 @@ Say "Installing dotnet";
 curl -ksSL -o /tmp/install-DOTNET.sh https://raw.githubusercontent.com/devizer/test-and-build/master/lab/install-DOTNET.sh; 
 export DOTNET_TARGET_DIR=/usr/share/dotnet; 
 set +e
+Say "DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER is '${DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER}'"
 bash /tmp/install-DOTNET.sh;
 set -e 
 ln -f -s ${DOTNET_TARGET_DIR}/dotnet /usr/local/bin/dotnet;  
