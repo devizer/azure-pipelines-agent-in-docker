699dcbcb5c0fc54d71220bd55b4a5486ebbfc467
devizer <devizer@users.github.com>
2020-03-13 12:50:58 +0200

    adding test runners

--- cross-platform/on-build/0a-install-test-runners.sh
+++ cross-platform/on-build/0a-install-test-runners.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 Say "Starting in '$(pwd)'. Content of cross-platform/bin/opt/* is below"
-pushd ../..
+pushd ..
 ls -la cross-platform/bin/opt/* || true
 tree . -h || true
 popd
