e9bf145446ca2550f0c247f17cac3ca5b619a048
devizer <devizer@users.github.com>
2020-03-13 13:54:45 +0200

    adding test runners

--- cross-platform/on-build/0b-install-test-runners.sh
+++ cross-platform/on-build/0b-install-test-runners.sh
@@ -2,5 +2,11 @@
 Say "Starting in '$(pwd)'. Content of cross-platform/bin/opt/* is below"
 pushd ..
 ls -la cross-platform/bin/opt/* || true
-tree . -h || true
+# tree . -h || true
+sudo mkdir -p /opt/net-test-runners
+cp -r bin/opt /opt
+tree /opt -h
+pushd /opt/net-test-runners
+bash link-unit-test-runners.sh
+popd
 popd
