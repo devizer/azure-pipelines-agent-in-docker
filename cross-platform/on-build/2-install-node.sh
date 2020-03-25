77654c2b6edab73f9a7b1be2fcbbc1ea070207ed
devizer <devizer@users.github.com>
2020-03-25 23:56:05 +0200

    yarn config set network-timeout 600000 -g

--- cross-platform/on-build/2-install-node.sh
+++ cross-platform/on-build/2-install-node.sh
@@ -2,3 +2,6 @@
 
 Say "Installing NodeJS LTS";
 script=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-and-nodejs.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash -s node;
+
+yarn config set network-timeout 600000 -g
+
