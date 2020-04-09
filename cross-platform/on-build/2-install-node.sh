c92a706af2fbcd7371a91dc539a2c495c31363cc
devizer <devizer@users.github.com>
2020-04-09 07:52:05 +0300

    nvm --lts instead of hardcoded version with error checking again

--- cross-platform/on-build/2-install-node.sh
+++ cross-platform/on-build/2-install-node.sh
@@ -5,5 +5,5 @@ su -c 'export HOME=/home/user; bash -e ../install-nvm-and-node-as-user.sh' user
 
 # script=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-and-nodejs.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash -s node;
 
-yarn config set network-timeout 600000 -g
+# yarn config set network-timeout 600000 -g
 
