0cecfcd8dfa6bf519d7365723dc2bc49a5912357
devizer <devizer@users.github.com>
2020-04-09 07:42:15 +0300

    nvm --lts instead of hardcoded version with error checking

--- cross-platform/on-build/2-install-node.sh
+++ cross-platform/on-build/2-install-node.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 Say "Installing NodeJS LTS";
-su -c 'export HOME=/home/user; bash ../install-nvm-and-node-as-user.sh' user
+su -c 'export HOME=/home/user; bash -e ../install-nvm-and-node-as-user.sh' user
 
 # script=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-and-nodejs.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash -s node;
 
