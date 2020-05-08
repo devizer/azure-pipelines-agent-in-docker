a4b60031e24599b000a367bb2f9c0e6e92a8bacd
devizer <devizer@users.github.io>
2020-05-08 18:12:46 +0300

    libcurl3 is optional for focal

--- cross-platform/disable-ipv6.sh
+++ cross-platform/disable-ipv6.sh
@@ -3,6 +3,6 @@ echo '
 net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 1
-' | sudo tee -a /etc/sysctl.conf
-sudo sysctl -p
+' | sudo tee -a /etc/sysctl.conf || true
+sudo sysctl -p || true
 
