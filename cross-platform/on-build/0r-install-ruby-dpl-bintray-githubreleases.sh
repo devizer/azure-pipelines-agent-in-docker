7640248af507d9fe1498b030539a41afd098c36c
devizer <devizer@users.github.io>
2020-05-09 00:44:51 +0300

    ruby-dev installation via qemu-static fails

--- cross-platform/on-build/0r-install-ruby-dpl-bintray-githubreleases.sh
+++ cross-platform/on-build/0r-install-ruby-dpl-bintray-githubreleases.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+echo ":ssl_verify_mode: 0" | tee -a ~/.gemrc
+Say "Installing ruby-dev via apt"
 sudo apt-get install -y ruby-dev
+Say "Installing dpl dpl-releases dpl-bintray via gem"
 sudo gem install dpl dpl-releases dpl-bintray
