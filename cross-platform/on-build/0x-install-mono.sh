c9104a12e03b1e0f57a5db72a278c57dc6dce9da
devizer <devizer@users.github.io>
2020-05-09 17:22:21 +0300

    what if focal?

--- cross-platform/on-build/0x-install-mono.sh
+++ cross-platform/on-build/0x-install-mono.sh
@@ -2,3 +2,9 @@
 script=https://raw.githubusercontent.com/devizer/test-and-build/master/lab/install-MONO.sh
 (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash; 
 sudo apt clean
+
+# strip mono
+Say "Strip /usr/lib/mono/**.so"
+pushd /usr/lib/mono 
+find -name '*.so' | xargs strip || true
+popd 
