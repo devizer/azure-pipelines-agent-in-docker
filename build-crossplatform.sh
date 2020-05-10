773128d3434886930193c6cc8eb9ec49b00b49ad
devizer <devizer@users.github.io>
2020-05-10 17:48:56 +0300

    /etc/environment for node and .NET core tools ***NO_CI***

--- build-crossplatform.sh
+++ build-crossplatform.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # work=$HOME/build/azure-pipeline-agent-in-docker; mkdir -p $(dirname $work); cd $(dirname $work); git clone https://github.com/devizer/azure-pipeline-agent-in-docker || true; cd azure-pipeline-agent-in-docker; git pull; time bash build-crossplatform.sh
 
-docker image rm -f devizervlad/azpa:latest
+docker image rm -f devizervlad/crossplatform-azure-pipelines-agent:latest
 # docker image rm -f $(docker image ls -aq)
 cd cross-platform
 time docker build -t devizervlad/crossplatform-azure-pipelines-agent:latest .
