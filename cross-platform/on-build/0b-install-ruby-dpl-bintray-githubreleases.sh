bbdf8ed2b669b0301f3a81790cc42f3c87ee611b
devizer <devizer@users.github.io>
2020-05-10 21:43:10 +0300

    skip dpl on jessie jessie ***NO_CI***

--- cross-platform/on-build/0b-install-ruby-dpl-bintray-githubreleases.sh
+++ cross-platform/on-build/0b-install-ruby-dpl-bintray-githubreleases.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 echo ":ssl_verify_mode: 0" | tee -a ~/.gemrc
-Say "Installing ruby-dev via apt"
-gem=$(apt-cache search gem | grep -E '^gem ' | awk '{print $1}')
-sudo apt-get install -y ruby-dev $gem
-Say "Installing dpl dpl-releases dpl-bintray via gem"
-sudo gem install dpl dpl-releases dpl-bintray
+source /etc/os-relase
+if [[ "$VERSION_ID" == "8" && "$ID" == "debian" ]]; then
+    Say "Skipping dpl, dpl-releases, dpl-bintray on Debian Jessie";
+else
+    Say "Installing ruby-dev via apt"
+    # gem=$(apt-cache search gem | grep -E '^gem ' | awk '{print $1}')
+    sudo apt-get install -y ruby-dev
+    Say "Installing dpl dpl-releases dpl-bintray via gem"
+    sudo gem install dpl dpl-releases dpl-bintray
+fi
