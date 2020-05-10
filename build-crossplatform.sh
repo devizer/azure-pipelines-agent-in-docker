b0d2cb9b72cfa967df1a380747734e9f794b2d37
devizer <devizer@users.github.io>
2020-05-10 18:08:11 +0300

    /etc/environment for node and .NET core tools ***NO_CI***

--- build-crossplatform.sh
+++ build-crossplatform.sh
@@ -2,6 +2,13 @@
 # work=$HOME/build/azure-pipeline-agent-in-docker; mkdir -p $(dirname $work); cd $(dirname $work); git clone https://github.com/devizer/azure-pipeline-agent-in-docker || true; cd azure-pipeline-agent-in-docker; git pull; time bash build-crossplatform.sh
 
 docker image rm -f devizervlad/crossplatform-azure-pipelines-agent:latest
+
+# build nunit and xunit test runners
+export XFW_VER=net47 NET_TEST_RUNNERS_INSTALL_DIR=/opt/net-test-runners; 
+export XFW_VER=net47 NET_TEST_RUNNERS_INSTALL_DIR=$(pwd)/cross-platform/bin/opt/net-test-runners;
+script=https://raw.githubusercontent.com/devizer/test-and-build/master/lab/NET-TEST-RUNNERS-build.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | sudo -E bash
+
+
 # docker image rm -f $(docker image ls -aq)
 cd cross-platform
 time docker build --build-arg BASE_IMAGE=focal -t devizervlad/crossplatform-azure-pipelines-agent:latest .
