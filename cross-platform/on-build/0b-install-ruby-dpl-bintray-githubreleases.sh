e046ad86e0794037748d0f50941cd16c5b759e46
devizer <devizer@users.github.io>
2020-05-10 21:18:29 +0300

    gem for jessie ***NO_CI***

--- cross-platform/on-build/0b-install-ruby-dpl-bintray-githubreleases.sh
+++ cross-platform/on-build/0b-install-ruby-dpl-bintray-githubreleases.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo ":ssl_verify_mode: 0" | tee -a ~/.gemrc
 Say "Installing ruby-dev via apt"
-sudo apt-get install -y ruby-dev
+gem=$(apt-cache search gem | grep -E '^gem ' | awk '{print $1}')
+sudo apt-get install -y ruby-dev $gem
 Say "Installing dpl dpl-releases dpl-bintray via gem"
 sudo gem install dpl dpl-releases dpl-bintray
