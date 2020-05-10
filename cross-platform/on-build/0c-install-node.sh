fbb483d349b22c61c3112f56dbdf5d438fbfd819
devizer <devizer@users.github.io>
2020-05-11 01:49:45 +0300

    fix: node on buster for qemu

--- cross-platform/on-build/0c-install-node.sh
+++ cross-platform/on-build/0c-install-node.sh
@@ -3,22 +3,32 @@
 # same as install git
 sudo apt-get install -y -qq build-essential libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip
 
-export NVM_DIR=/opt/nvm
 Say "Installing NodeJS LTS as $(whoami)";
-mkdir -p $NVM_DIR
-chown -R user:user $NVM_DIR
-echo $NVM_DIR | sudo tee /etc/NVM_DIR
 
-su -c 'export HOME=/home/user; export NVM_DIR='$NVM_DIR'; bash -e ../install-nvm-and-node-as-user.sh' user
+source /etc/os-release
+if [[ "$VERSION_ID" == "10" && "$ID" == "debian" ]]; then
+    # network does not work properly for Buster over qemu
+    Say "Installing Node LTS using custom installer"
+    script=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-and-nodejs.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash -s node
+    node_path=$(dirname $(ls /opt/node/node*/bin/node))
+    Say "New node_path via custom node installer is: [$node_path]"
+else
+    export NVM_DIR=/opt/nvm
+    mkdir -p $NVM_DIR
+    chown -R user:user $NVM_DIR
+    echo $NVM_DIR | sudo tee /etc/NVM_DIR
+    su -c 'export HOME=/home/user; export NVM_DIR='$NVM_DIR'; bash -e ../install-nvm-and-node-as-user.sh' user
+    
+    Say "Looking for node path as $(whoami) for /etc/environment"
+    [[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"
+    node_path=$(dirname `nvm which current || true` || true)
+fi
 
-Say "Looking for node path as $(whoami) for /etc/environment"
+Say "Store node path for /etc/environment: [$node_path]"
 source /etc/environment
-[[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"
-node_path=$(dirname `nvm which current || true` || true)
 new_PATH="$PATH:$node_path"
 sed '/PATH/d' /etc/environment
 printf "\nPATH=${new_PATH}\n" | sudo tee /etc/environment
-
 
 # script=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-and-nodejs.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash -s node;
 
