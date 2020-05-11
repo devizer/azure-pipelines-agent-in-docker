6fb6e4538ee029646e549303a407d7f229697016
devizer <devizer@users.github.io>
2020-05-11 03:32:43 +0300

    apt-get clean for jessie

--- cross-platform/on-build/0x-install-mono.sh
+++ cross-platform/on-build/0x-install-mono.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 script=https://raw.githubusercontent.com/devizer/test-and-build/master/lab/install-MONO.sh
 (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash; 
-sudo apt clean
+sudo apt-get clean
 
 # strip mono
 Say "Strip /usr/lib/mono/**.so"
