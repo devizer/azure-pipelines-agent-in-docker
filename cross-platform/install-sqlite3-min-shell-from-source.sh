a58f838b5f8455ea443099652225dd6b52270799
devizer <devizer@users.github.io>
2020-05-11 20:27:24 +0300

    sqlite 3.31.1

---
+++ cross-platform/install-sqlite3-min-shell-from-source.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# apt-get install unzip binutils bsdutils build-essential wget -y
+set -e
+url=https://www.sqlite.org/2020/sqlite-amalgamation-3310100.zip
+work=$HOME/build/sqlite3-src
+mkdir -p $work
+pushd $work
+wget --no-check-certificate -O _sqlite3.src.zip $url || curl -ksSL -o _sqlite3.src.zip $url
+unzip -o _sqlite3.src.zip
+cd sqlite*
+time gcc -O2 shell.c sqlite3.c -lpthread -ldl -o sqlite3
+strip sqlite3
+echo "sqlite3 version: $(./sqlite3 /tmp/temp-sqllite.db 'select sqlite_version();')"
+rm -f /tmp/temp-sqllite.db || true
+sudo mv sqlite3 /usr/local/bin/sqlite3 2>/dev/null || mv sqlite3 /usr/local/bin/sqlite3
+popd
+rm -rf $work
--- cross-platform/on-build/5-install-db-clients.sh
+++ cross-platform/on-build/5-install-db-clients.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+Say "Installing mysql-client and postgresql-client"
 sudo apt-get clean
 myclient1=$(apt-cache search default-mysql-client | grep -E '^default-mysql-client ' | awk '{print $1}')
 myclient2=$(apt-cache search mysql-client | grep -E '^mysql-client ' | awk '{print $1}')
-try-and-retry sudo apt-get install $myclient1 $myclient2 postgresql-client sqlite3 -y
-sudo apt-get clean
+try-and-retry sudo apt-get install $myclient1 $myclient2 postgresql-client -y
 
+Say "Install sqlite3 (minimum) shell from source"
+bash -e ../install-sqlite3-min-shell-from-source.sh
+sudo apt-get clean
