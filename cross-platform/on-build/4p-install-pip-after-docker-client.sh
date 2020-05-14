e10859d6fcf796ae19ad376805ae3f2d801481da
devizer <devizer@users.github.io>
2020-05-14 20:38:08 +0300

    better /VERSION

---
+++ cross-platform/on-build/4p-install-pip-after-docker-client.sh
@@ -0,0 +1,7 @@
+﻿#!/usr/bin/env bash
+if [[ -z "$(command -v python3 || true)" ]]; then
+    smart-apt-install python3 || true
+fi
+script="https://bootstrap.pypa.io/get-pip.py"
+wget -q -nv --no-check-certificate -O "/tmp/get-pip.py" $script 2>/dev/null || curl -ksSL $script -o "/tmp/get-pip.py"
+python3 /tmp/get-pip.py --user
