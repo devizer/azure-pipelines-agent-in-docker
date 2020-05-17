1416b1b4ee9f0b39bf1d11d602914ef82fa4e189
devizer <devizer@users.github.io>
2020-05-17 23:52:58 +0300

    fix BOM, added commit and branch to version

--- cross-platform/on-build/0i-install-dotnet.sh
+++ cross-platform/on-build/0i-install-dotnet.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 Say "Installing dotnet"; 
+export DOTNET_TARGET_DIR=/usr/share/dotnet 
 curl -ksSL -o /tmp/install-DOTNET.sh https://raw.githubusercontent.com/devizer/test-and-build/master/lab/install-DOTNET.sh; 
-export DOTNET_TARGET_DIR=/usr/share/dotnet; 
 set +e
 Say "DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER is '${DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER}'"
 export DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0
