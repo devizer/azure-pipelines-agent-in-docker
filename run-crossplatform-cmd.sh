ac3a4e8650af9fdf9ac30cc0a3d653ad28f57e80
devizer <devizer@users.github.io>
2020-05-18 15:27:24 +0300

    pull & test x86 and free Space for arm and arm64 Again2 (job names and last system stat)

--- run-crossplatform-cmd.sh
+++ run-crossplatform-cmd.sh
@@ -9,6 +9,11 @@ sha=$(echo $json | jq -r '.manifests[] | if .platform.architecture == "'$arch'" 
 if [[ "$sha" == "" ]]; then
   Say "Skipping: architecture [$arch] not found for ${TAG}"
 else
-  Say "$info for [${TAG}] running [${arch}]"
+  Say "${TAG}:$info Free space"
+  docker image rm -f $(docker image ls | grep devizervlad/crossplatform-azure-pipelines-agent | awk '{print $3}') >/dev/null 2>/dev/null
+  Say "${TAG}:$info Pull devizervlad/crossplatform-azure-pipelines-agent:${TAG}@${sha} for [${TAG}] running [${arch}]"
+  docker pull "devizervlad/crossplatform-azure-pipelines-agent:${TAG}@${sha}" >/dev/null
+  Say "${TAG}:$info for [${TAG}] running [${arch}]"
   docker run -t --rm "devizervlad/crossplatform-azure-pipelines-agent:${TAG}@${sha}" bash -c "$cmd" || exit 1
+  Say "${TAG}:$info finished"
 fi
