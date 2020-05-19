06c26cd6f52c6459accc48be5e4fde4068d7f1bb
devizer <devizer@users.github.io>
2020-05-19 17:36:37 +0300

    fix: clean git-lsf source dependencies again

--- cross-platform/on-build/0g-install-GO-and-GIT-LFS.sh
+++ cross-platform/on-build/0g-install-GO-and-GIT-LFS.sh
@@ -14,7 +14,7 @@ git clone https://github.com/git-lfs/git-lfs
 cd git-lfs*
 git checkout $GIT_LFS_VER
 Say "Downloading dependencies for GIT-LFS $GIT_LFS_VER"
-go mod download -x
+time try-and-retry go mod download
 Say "Building GIT-LFS $GIT_LFS_VER"
 time make -B
 sudo mv bin/git-lfs /usr/local/bin/git-lfs
