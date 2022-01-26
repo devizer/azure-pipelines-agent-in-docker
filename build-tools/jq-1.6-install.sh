37c91315b31d66d4fd2d034f7994abd6737f7a2a
devizer <devizer@github.users>
2022-01-26 18:26:07 +0200

    added jq 1.6 ***NO_CI***

---
+++ build-tools/jq-1.6-install.sh
@@ -0,0 +1,29 @@
+set -eu
+apt-get install make autoconf build-essential libtool sudo wget curl htop mc cmake pv jq p7zip xz-utils -y -qq
+
+INSTALL_PREFIX="${INSTALL_PREFIX:-/usr/local}"
+
+work=$HOME/src/jq-1.6
+mkdir -p $work
+pushd $work
+curl -kSL -o jq-1.6.tar.gz https://github.com/stedolan/jq/releases/download/jq-1.6/jq-1.6.tar.gz
+tar xzf jq-1.6.tar.gz
+rm -f jq-1.6.tar.gz
+cd jq*
+autoreconf -fi
+./configure --prefix="${INSTALL_PREFIX}" --disable-maintainer-mode
+sudo make -j install
+if [[ -d "${INSTALL_PREFIX}/bin" ]]; then
+  pushd "${INSTALL_PREFIX}/bin"
+    strip jq onig*
+  popd
+fi
+if [[ -d "${INSTALL_PREFIX}/lib" ]]; then
+  pushd "${INSTALL_PREFIX}/lib"
+    strip *libjq*so* *libonig*so*
+  popd
+fi
+
+popd
+
+rm -rf "$work"
