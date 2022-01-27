383b133013a1950c56db91b4afb5b8779f9c650f
devizer <devizer@github.users>
2022-01-27 16:35:00 +0200

    new automake on debian 7 v3

--- build-tools/install-jq-1.6.sh
+++ build-tools/install-jq-1.6.sh
@@ -10,9 +10,13 @@ curl -kSL -o jq-1.6.tar.gz https://github.com/stedolan/jq/releases/download/jq-1
 tar xzf jq-1.6.tar.gz
 rm -f jq-1.6.tar.gz
 cd jq*
+Say "autoreconf for jq on [$(uname -m)]"
 autoreconf -fi
+Say "configure for jq on [$(uname -m)]"
 ./configure --prefix="${INSTALL_PREFIX}" --disable-maintainer-mode --with-oniguruma=builtin
+Say "make install for jq on [$(uname -m)]"
 sudo make -j install
+Say "strip for jq on [$(uname -m)]"
 if [[ -d "${INSTALL_PREFIX}/bin" ]]; then
   pushd "${INSTALL_PREFIX}/bin"
     strip jq onig* || true
