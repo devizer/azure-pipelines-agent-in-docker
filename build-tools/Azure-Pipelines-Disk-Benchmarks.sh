31ae9e9dd1cf32ac7af98820c8f4825b345d2d10
devizer <devizer@github.users>
2022-02-07 00:33:44 +0200

    Disk Benchmarks ***NO_CI***

--- build-tools/Azure-Pipelines-Disk-Benchmarks.sh
+++ build-tools/Azure-Pipelines-Disk-Benchmarks.sh
@@ -1,7 +1,10 @@
 set -eu; set -o pipefail
 
-script=https://raw.githubusercontent.com/devizer/test-and-build/master/install-build-tools-bundle.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | TARGET_DIR=/usr/local/bin bash 
+script=https://raw.githubusercontent.com/devizer/test-and-build/master/install-build-tools-bundle.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | TARGET_DIR=/usr/local/bin bash >/dev/null
 
+Say "Default / (the root)"
 time sudo File-IO-Benchmark 'ROOT' /    12G 60 15 | tee "$SYSTEM_ARTIFACTSDIRECTORY/default-root-benchmark.console.txt"
+
+Say "Default /mnt"
 time sudo File-IO-Benchmark '/MNT' /mnt 12G 60 15 | tee "$SYSTEM_ARTIFACTSDIRECTORY/default-mnt-benchmark.console.txt"
 
