77c523b699f72339def360aa73ce1f285e6d3347
devizer <devizer@github.users>
2022-02-13 12:01:41 +0200

    8th termux micro proot ***NO_CI***

--- build-tools/Prepare-proot.sh
+++ build-tools/Prepare-proot.sh
@@ -11,6 +11,7 @@ SYSTEM_ARTIFACTSDIRECTORY="${SYSTEM_ARTIFACTSDIRECTORY:-/transient-builds}"
 
 sudo apt-get install rsync pv sshpass jq qemu-user-static -y -qq >/dev/null
 script=https://raw.githubusercontent.com/devizer/test-and-build/master/install-build-tools-bundle.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | TARGET_DIR=/usr/local/bin bash > /dev/null
+Say --Reset-Stopwatch
 smart-apt-install rsync pv sshpass jq qemu-user-static -y -qq >/dev/null
 
 Say "Registering binary formats for qemu-user-static"
