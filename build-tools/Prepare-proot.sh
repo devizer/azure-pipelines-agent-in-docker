29a2c935adc9e98925c84fcd23efa83e49e590af
devizer <devizer@github.users>
2022-02-13 15:01:14 +0200

    12th termux micro proot ***NO_CI***

--- build-tools/Prepare-proot.sh
+++ build-tools/Prepare-proot.sh
@@ -50,7 +50,7 @@ function replace_links_to_relative() {
   popd > /dev/null
 }
 
-function format_Kb() {
+function format_Integer() {
   local num="$1";
   local str="$(printf "%'.0f" "${num}")"
   echo $str;
@@ -192,7 +192,7 @@ sudo rm -rf $work/var/log/* $work/var/tmp/*
   local xzSize="$(stat --printf="%s" "${xzFile}")"; xzSize=$((xzSize/1024)) 
   local plainSize="$(du --max-depth=0 "$work" | awk '{print $1}')";
   local sizesFile="$SYSTEM_ARTIFACTSDIRECTORY/sizes.txt"
-  printf "| %-40s | %12s | %12s |\n" "$(basename ${xzFile})" "$(format_Kb "$xzSize")" "$(format_Kb "plainSize")" >> "$sizesFile"
+  printf "| %-40s | %12s | %12s |\n" "$(basename ${xzFile})" "$(format_Integer "$xzSize")" "$(format_Kb "$plainSize")" >> "$sizesFile"
   cat "$sizesFile"
   Say "Size of ${xzFile}: $(printf "%'.0f" ${xzSize}) KBytes"
   popd
