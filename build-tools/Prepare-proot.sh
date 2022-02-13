ac07bc4bff2e2a859e9a7e61ef3f6775538e76c9
devizer <devizer@github.users>
2022-02-13 14:32:09 +0200

    9th termux micro proot ***NO_CI***

--- build-tools/Prepare-proot.sh
+++ build-tools/Prepare-proot.sh
@@ -7,7 +7,7 @@
 set -e; set -u; set -o pipefail
 
 SYSTEM_ARTIFACTSDIRECTORY="${SYSTEM_ARTIFACTSDIRECTORY:-/transient-builds}"
-
+export LC_ALL=en_US.utf8
 
 sudo apt-get install rsync pv sshpass jq qemu-user-static -y -qq >/dev/null
 script=https://raw.githubusercontent.com/devizer/test-and-build/master/install-build-tools-bundle.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | TARGET_DIR=/usr/local/bin bash > /dev/null
