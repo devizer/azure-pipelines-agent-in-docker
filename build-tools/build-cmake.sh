ce597b8121934b7a3f2487ef12adaf9e28e1037a
devizer <devizer@github.users>
2022-02-17 17:52:41 +0200

    cmake aarch64 11 on azure pipelines ***NO_CI***

--- build-tools/build-cmake.sh
+++ build-tools/build-cmake.sh
@@ -3,7 +3,7 @@ machine=$(uname -m);
 [[ $machine == aarch64 ]] && machine=arm64v8
 [[ $machine == armv* ]] && machine=arm32v7
 cpus=$(cat /proc/cpuinfo | grep -E '^(P|p)rocessor' | wc -l)
-GCCVER=${GCCVER:11}; # [[ $machine == arm32v7 ]] && GCCVER=5
+GCCVER=${GCCVER:-11}; # [[ $machine == arm32v7 ]] && GCCVER=5
 Say "Processors: $cpus, GCC $GCCVER"
 export GCC_INSTALL_VER=$GCCVER GCC_INSTALL_DIR=/usr/local; script="https://master.dl.sourceforge.net/project/gcc-precompiled/install-gcc.sh?viasf=1"; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash
 
