a551c15090866c98f3bc8f5373ea61613b8990c0
devizer <devizer@github.users>
2022-03-09 04:17:35 +0200

    gnu tools, 2nd 5*arm32v5L  ***NO_CI***

--- build-tools/install-the-gnu-tools.sh
+++ build-tools/install-the-gnu-tools.sh
@@ -26,7 +26,7 @@ install-a-gnu-tool "sed-4.8"         "https://ftp.gnu.org/gnu/sed/sed-4.8.tar.gz
 install-a-gnu-tool "automake-1.16.5" "https://ftp.gnu.org/gnu/automake/automake-1.16.5.tar.gz"
 install-a-gnu-tool "m4-1.4.19"       "https://ftp.gnu.org/gnu/m4/m4-1.4.19.tar.gz"
 install-a-gnu-tool "autoconf-2.71"   "https://ftp.gnu.org/gnu/autoconf/autoconf-2.71.tar.gz"
-install-a-gnu-tool "libtool-2.4.6"   "https://ftp.gnu.org.ua/gnu/libtool/libtool-2.4.6.tar.gz"
+install-a-gnu-tool "libtool-2.4.6"   "https://ftp.gnu.org/gnu/libtool/libtool-2.4.6.tar.gz"
 install-a-gnu-tool "make-4.3"        "https://ftp.gnu.org/gnu/make/make-4.3.tar.gz"
 install-a-gnu-tool "gawk-5.1.1"      "https://ftp.gnu.org/gnu/gawk/gawk-5.1.1.tar.gz"
 install-a-gnu-tool "grep-3.7"        "https://ftp.gnu.org/gnu/grep/grep-3.7.tar.gz"
