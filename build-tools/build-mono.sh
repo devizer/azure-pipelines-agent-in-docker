d06e8975522f839d1475edf19afb071c1cfb6da8
devizer <devizer@github.users>
2022-03-18 10:13:13 +0200

    7th build-mono.sh, jemalloc=no ***NO_CI***

--- build-tools/build-mono.sh
+++ build-tools/build-mono.sh
@@ -27,14 +27,15 @@ function Add-LD-Path() {
 # Say "Install gcc 11.2"
 # export GCC_INSTALL_VER=11 GCC_INSTALL_DIR=/usr/local; script="https://master.dl.sourceforge.net/project/gcc-precompiled/install-gcc.sh?viasf=1"; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash
 Say "Install cmake and gnu build tools"
-script="https://master.dl.sourceforge.net/project/gcc-precompiled/build-tools/Install-Build-Tools.sh?viasf=1"; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | INSTALL_DIR=/usr/local TOOLS="gnu-tools cmake git" bash
+script="https://master.dl.sourceforge.net/project/gcc-precompiled/build-tools/Install-Build-Tools.sh?viasf=1"; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | INSTALL_DIR=/usr TOOLS="gnu-tools cmake git" bash
 
 # Add-LD-Path /usr/local/lib /usr/local/lib64
 
 export DEBIAN_FRONTEND=noninteractive
 export TRANSIENT_BUILDS=/transient-builds
-MONO_VER="${MONO_VER:-6.12.0.122}"
-export MONO_HOME=/opt/mono-${MONO_VER}
+export MONO_VER="${MONO_VER:-6.12.0.122}"
+# export MONO_HOME=/opt/mono-${MONO_VER}
+export MONO_HOME=/usr/local
 
 apt-get -y install lsof binutils git \
    build-essential autoconf pkg-config \
@@ -59,21 +60,24 @@ export CXXFLAGS="$CFLAGS"
 sed -i 's/git:\/\//https:\/\//g' mono/utils/jemalloc/SUBMODULES.json
 cat mono/utils/jemalloc/SUBMODULES.json
 
+# --disable-boehm --with-gc=none --with-sgen=yes
 time ./autogen.sh --prefix="$MONO_HOME" --with-jemalloc=no --disable-werror --enable-dtrace=no \
-  --with-profile2=no --disable-boehm --disable-maintainer-mode --disable-dependency-tracking \
-  --with-gc=none --with-sgen=yes --with-mcs-docs=no \
-  --with-static_mono=no --with-shared_mono=yes \
+  --with-profile2=no --disable-maintainer-mode --disable-dependency-tracking \
+  --with-libgc=included --with-sgen=no --disable-llvm --disable-dtrace \
+  --with-mcs-docs=no \
+  --with-static_mono=yes --with-shared_mono=no \
   --disable-gtk-doc --with-mcs-docs=no --enable-nls=no --disable-boehm \
   --with-ikvm-native=no --enable-minimal=profiler,attach,com \
   |& tee autogen.log
 
 time (make -j$(nproc) && make install)
+echo "make install complete"
 $MONO_HOME/bin/mono --version
 
 
 cd "$MONO_HOME"
 Say "pack [$(pwd)] release as gz"
-artifact="$SYSTEM_ARTIFACTSDIRECTORY/mono-6.12.0.122-$machine"
+artifact="$SYSTEM_ARTIFACTSDIRECTORY/mono-${MONO_VER}-$machine"
 tar cf - . | gzip -9 > ${artifact}.tar.gz
 Say "pack [$(pwd)] release as xz"
 tar cf - . | xz -z -9 -e > ${artifact}.tar.xz
@@ -81,4 +85,3 @@ build_all_known_hash_sums ${artifact}.tar.xz
 build_all_known_hash_sums ${artifact}.tar.gz
 
 Say "Done"
-# kill $pid || true
