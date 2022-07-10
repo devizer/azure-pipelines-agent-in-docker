53f7786e3f542e9f6e8af8b91dce629606960e25
devizer <devizer@github.io>
2022-07-10 10:29:53 +0300

    added raspbian armv6 wheezy ***NO_CI***

---
+++ build-tools/Prepare-Docker-Images.sh
@@ -0,0 +1,41 @@
+# https://github.com/nmilosev/termux-fedora/blob/master/termux-fedora.sh
+# image="debian:8"
+# image="multiarch/debian-debootstrap:arm64-jessie"
+# image="arm64v8/debian:8"
+# KEY=rootfs-debian-8-arm64
+# [[ "$(command -v jq)" == "" ]] && apt-get install jq -y
+set -e; set -u; set -o pipefail
+
+SYSTEM_ARTIFACTSDIRECTORY="${SYSTEM_ARTIFACTSDIRECTORY:-/transient-builds}"
+export LC_ALL=en_US.utf8
+
+sudo apt-get install rsync pv sshpass jq qemu-user-static -y -qq >/dev/null
+script=https://raw.githubusercontent.com/devizer/test-and-build/master/install-build-tools-bundle.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | TARGET_DIR=/usr/local/bin bash > /dev/null
+Say --Reset-Stopwatch
+smart-apt-install rsync pv sshpass jq qemu-user-static -y -qq >/dev/null
+
+Say "Registering binary formats for qemu-user-static"
+docker run --rm --privileged multiarch/qemu-user-static:register --reset >/dev/null
+# docker buildx imagetools inspect --raw "$image" | jq
+
+for f in build-gcc-utilities.sh; do
+  try-and-retry curl -kSL -o /tmp/$f https://raw.githubusercontent.com/devizer/NetCore.CaValidationLab/master/4gcc/$f
+done
+
+
+
+src=$(pwd)
+work=/transient-builds/temp-armv6
+mkdir -p $work
+rm -rf $work/*
+cd $work
+cp $src/Raspbian-Images/Dockerfile .
+Say "Downloading [$IMAGE_URL]"
+file="$(basename "$IMAGE_URL")"
+try-and-retry curl -kSL -o "$file" "$IMAGE_URL"
+Say "Extracting [$file]"
+7z x -y "$file"
+# rm -f "$file"
+ls -la
+ls -la *.img
+
