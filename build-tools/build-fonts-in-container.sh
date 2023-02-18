f1a212afd20b4a11cd4d225b6ac98a2f5eb78eac
devizer <devizer@users.github.io>
2023-02-18 21:43:59 +0200

    fetch mono fonts v8 ***NO_CI***

--- build-tools/build-fonts-in-container.sh
+++ build-tools/build-fonts-in-container.sh
@@ -1,8 +1,12 @@
 export DEBIAN_FRONTEND=noninteractive
 echo "SYSTEM_ARTIFACTSDIRECTORY: [$SYSTEM_ARTIFACTSDIRECTORY]"
 time apt-get update
-time apt-get install less fontconfig mc htop ncdu p7zip -y
+time apt-get install sudo less fontconfig mc htop ncdu p7zip software-properties-common sudo aria2 curl -y
 
-time apt-get install -y $(apt-cache search font | awk '{print $1}' | grep font | grep -v "fontforge-nox\|scalable-cyrfonts-tex")
+add-apt-repository ppa:apt-fast/stable
+apt-get update
+apt-get -y install apt-fast
+
+time apt-fast install -y $(apt-cache search font | awk '{print $1}' | grep font | grep -v "fontforge-nox\|scalable-cyrfonts-tex")
 
 fc-list :spacing=100 | tee $SYSTEM_ARTIFACTSDIRECTORY/mono-fonts-raw.txt
