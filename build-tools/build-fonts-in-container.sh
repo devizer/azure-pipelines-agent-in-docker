b7cf052490036c915dd9fc36eb30cd3e9345c2f3
devizer <devizer@users.github.io>
2023-02-18 21:31:38 +0200

    fetch mono fonts v3 ***NO_CI***

--- build-tools/build-fonts-in-container.sh
+++ build-tools/build-fonts-in-container.sh
@@ -1,4 +1,7 @@
+echo "SYSTEM_ARTIFACTSDIRECTORY: [$SYSTEM_ARTIFACTSDIRECTORY]"
 time apt-get update
 time apt-get install less fontconfig mc htop ncdu p7zip -y
 
 time apt-get install -y $(apt-cache search font | awk '{print $1}' | grep font | grep -v "fontforge-nox\|scalable-cyrfonts-tex")
+
+fc-list :spacing=100 | tee $SYSTEM_ARTIFACTSDIRECTORY/mono-fonts-raw.txt
