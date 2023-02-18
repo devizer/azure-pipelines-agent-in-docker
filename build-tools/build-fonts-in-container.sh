b05384db6fe41a7a89e6748a17dc283ba4581c80
devizer <devizer@users.github.io>
2023-02-18 21:46:35 +0200

    fetch mono fonts v9 ***NO_CI***

--- build-tools/build-fonts-in-container.sh
+++ build-tools/build-fonts-in-container.sh
@@ -1,12 +1,13 @@
 export DEBIAN_FRONTEND=noninteractive
 echo "SYSTEM_ARTIFACTSDIRECTORY: [$SYSTEM_ARTIFACTSDIRECTORY]"
 time apt-get update
-time apt-get install sudo less fontconfig mc htop ncdu p7zip software-properties-common sudo aria2 curl -y
+time apt-get install sudo less fontconfig mc htop ncdu p7zip software-properties-common sudo aria2 curl -y | grep "Setting"
 
 add-apt-repository ppa:apt-fast/stable
 apt-get update
-apt-get -y install apt-fast
+apt-get -y install apt-fast | grep "Setting"
 
+Say "APT FAST"
 time apt-fast install -y $(apt-cache search font | awk '{print $1}' | grep font | grep -v "fontforge-nox\|scalable-cyrfonts-tex")
 
 fc-list :spacing=100 | tee $SYSTEM_ARTIFACTSDIRECTORY/mono-fonts-raw.txt
