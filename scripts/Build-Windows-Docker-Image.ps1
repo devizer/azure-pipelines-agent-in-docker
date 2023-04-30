f3b8b0db2bd8e83d2face3bb8f7fb4a1043d9db8
devizer <devizer@users.github.io>
2023-05-01 02:49:48 +0300

    8 SQL Server Docker Image ***NO_CI***

---
+++ scripts/Build-Windows-Docker-Image.ps1
@@ -0,0 +1,14 @@
+$tag="ltsc2019"
+$image="devizervlad/sqlserver-archive"
+pushd SqlDockerContext 
+
+Copy-Item -Path C:\SQL\ -Destination . -Recurse
+
+echo "Starting SQL Server..." >> BootstrapSqlServer.cmd
+echo "dir /b /s"              >> BootstrapSqlServer.cmd
+
+& docker build --build-arg TAG=$tag -t "$($image):$($imageTag)" .
+& docker run -t "$($image):$($imageTag)"
+
+popd
+
