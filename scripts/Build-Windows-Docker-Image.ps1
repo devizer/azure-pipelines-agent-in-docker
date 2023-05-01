a548cf61dfb1b0ec2a1ff60012d8787184d94dda
devizer <devizer@users.github.io>
2023-05-01 06:40:07 +0300

    SQL Server Docker Image ***NO_CI***, Reg-4

--- scripts/Build-Windows-Docker-Image.ps1
+++ scripts/Build-Windows-Docker-Image.ps1
@@ -10,12 +10,13 @@ pushd SqlDockerContext
 & regedit /e /s C:\SQL\SqlServer-32.reg "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\MSSQLServer"
 & regedit /e /s C:\SQL\SqlServer-64.reg "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSSQLServer"
 
-& regedit 
+# & regedit 
 Copy-Item -Path "C:\SQL\*" -Destination . -Recurse
 
 $sqlfull=(Get-ChildItem -Path C:\SQL -Filter sqlservr.exe -Recurse -ErrorAction SilentlyContinue -Force)[0].FullName
 $mdf=(Get-ChildItem -Path C:\SQL -Filter master.mdf -Recurse -ErrorAction SilentlyContinue -Force | Where { -not ($_.FullName -like "*Template*") } )[0].FullName
 $ldf=(Get-ChildItem -Path C:\SQL -Filter mastlog.ldf -Recurse -ErrorAction SilentlyContinue -Force | Where { -not ($_.FullName -like "*Template*") } )[0].FullName
+$errlog=(Get-ChildItem -Path C:\SQL -Filter ERRORLOG -Recurse -ErrorAction SilentlyContinue -Force | Where { -not ($_.FullName -like "*Template*") } )[0].FullName
 $sqlpath=[System.IO.Path]::GetDirectoryName($sqlfull)
 $sqlexe=[System.IO.Path]::GetFileName($sqlfull)
 echo "FULL: [$sqlfull]"
@@ -23,13 +24,15 @@ echo "PATH: [$sqlpath]"
 echo "EXE:  [$sqlexe]"
 echo "master mdf: [$mdf]"
 echo "master ldf: [$ldf]"
+echo "ERRORLOG: [$errlog]"
 
 
 echo "Echo Starting SQL Server..." >> BootstrapSqlServer.cmd
-echo "regedit /i /s C:\SQL\SqlServer-32.reg"
-echo "regedit /i /s C:\SQL\SqlServer-64.reg"
+echo "regedit /i /s C:\SQL\SqlServer-32.reg" >> BootstrapSqlServer.cmd
+echo "regedit /i /s C:\SQL\SqlServer-64.reg" >> BootstrapSqlServer.cmd
 echo "cd $sqlpath"            >> BootstrapSqlServer.cmd
-echo "$sqlexe -c -n -d $mdf -l $ldf -e C:\SQL\ERRORLOG" >> BootstrapSqlServer.cmd
+echo "del $errlog"            >> BootstrapSqlServer.cmd
+echo "$sqlexe -c -n -d $mdf -l $ldf -e$errlog" >> BootstrapSqlServer.cmd
 # echo "dir /b /s"              >> BootstrapSqlServer.cmd
 
 echo "BootstrapSqlServer.cmd CONTENT IS"
