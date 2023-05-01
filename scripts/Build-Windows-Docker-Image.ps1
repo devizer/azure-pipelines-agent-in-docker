be1d5cb462ab76ad41a0f40993fa67dc5a9cab31
devizer <devizer@users.github.io>
2023-05-01 03:42:49 +0300

    14 SQL Server Docker Image ***NO_CI***, BootstrapSqlServer.cmd

--- scripts/Build-Windows-Docker-Image.ps1
+++ scripts/Build-Windows-Docker-Image.ps1
@@ -7,8 +7,25 @@ pushd SqlDockerContext
 
 Copy-Item -Path "C:\SQL\*" -Destination . -Recurse
 
+$sqlfull=(Get-ChildItem -Path C:\SQL -Filter sqlservr.exe -Recurse -ErrorAction SilentlyContinue -Force)[0].FullName
+$mdf=(Get-ChildItem -Path C:\SQL -Filter master.mdf -Recurse -ErrorAction SilentlyContinue -Force)[0].FullName
+$ldf=(Get-ChildItem -Path C:\SQL -Filter mastlog.ldf -Recurse -ErrorAction SilentlyContinue -Force)[0].FullName
+$sqlpath=[System.IO.Path]::GetDirectoryName($sqlfull)
+$sqlexe=[System.IO.Path]::GetFileName($sqlfull)
+echo "FULL: [$sqlfull]"
+echo "PATH: [$sqlpath]"
+echo "EXE:  [$sqlexe]"
+echo "master mdf: [$mdf]"
+echo "master ldf: [$ldf]"
+
+
 echo "Starting SQL Server..." >> BootstrapSqlServer.cmd
+echo "cd $sqlpath"            >> BootstrapSqlServer.cmd
+echo "$sqlexe -d $mdf -l $ldf -e C:\SQL\ERRORLOG" >> BootstrapSqlServer.cmd
 echo "dir /b /s"              >> BootstrapSqlServer.cmd
+
+echo "BootstrapSqlServer.cmd CONTENT IS"
+cat BootstrapSqlServer.cmd
 
 & docker build --build-arg TAG=$tag -t "$($image):$($imageTag)" .
 & docker run -t "$($image):$($imageTag)"
