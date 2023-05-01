b50ec6203a1070d2c26738ff3e4a57d2ce1484e0
devizer <devizer@users.github.io>
2023-05-01 17:37:04 +0300

    SQL Server Docker Image ***NO_CI***, Reg-6, vcredist

---
+++ scripts/SqlDockerContext/Install-vcredist.cmd
@@ -0,0 +1,10 @@
+echo Installing choco ...
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+@echo off
+for %%v in (vcredist2008 vcredist2005 vcredist2010) DO (
+  echo Installing %%v
+  choco install -my %%v
+)
+
+
