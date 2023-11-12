36e0ab64775d3c923d6dd4a8acf2f497a47bb277
devizer <devizer@users.github.io>
2023-11-13 00:37:00 +0200

    3rd: added debian 12, ubuntu 22.04 and 23.10 with openssl 1.1.1 (also skip git on new os) ***NO_CI***

--- cross-platform/on-build/0a-Install-7z-git-bash.sh
+++ cross-platform/on-build/0a-Install-7z-git-bash.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-export INSTALL_DIR=/usr/local TOOLS="bash git jq 7z nano"; script="https://master.dl.sourceforge.net/project/gcc-precompiled/build-tools/Install-Build-Tools.sh?viasf=1"; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash
+set -eu
+test -f /etc/os-release && source /etc/os-release
+OS_VER="${ID:-}:${VERSION_ID:-}"
+TOOLS="bash git jq 7z nano"
+if [[ "${OS_VER:-}" == "debian:12" || "${OS_VER:-}" == "ubuntu:22"* || "${OS_VER:-}" == "ubuntu:23"* || "${OS_VER:-}" == "ubuntu:24"* ]]; then
+  TOOLS="bash jq 7z nano"
+fi
+export TOOLS
+
+export INSTALL_DIR=/usr/local; script="https://master.dl.sourceforge.net/project/gcc-precompiled/build-tools/Install-Build-Tools.sh?viasf=1"; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash
