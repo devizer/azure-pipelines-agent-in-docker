9a439cac1254ed20694e9bccba3f48db8149d236
devizer <devizer@users.github.io>
2023-11-13 00:38:47 +0200

    4th: added debian 12, ubuntu 22.04 and 23.10 with openssl 1.1.1 (also skip git on new os) ***NO_CI***

--- cross-platform/on-build/0a-Install-7z-git-bash.sh
+++ cross-platform/on-build/0a-Install-7z-git-bash.sh
@@ -4,7 +4,7 @@ test -f /etc/os-release && source /etc/os-release
 OS_VER="${ID:-}:${VERSION_ID:-}"
 TOOLS="bash git jq 7z nano"
 if [[ "${OS_VER:-}" == "debian:12" || "${OS_VER:-}" == "ubuntu:22"* || "${OS_VER:-}" == "ubuntu:23"* || "${OS_VER:-}" == "ubuntu:24"* ]]; then
-  TOOLS="bash jq 7z nano"
+  TOOLS="jq 7z"
 fi
 export TOOLS
 
