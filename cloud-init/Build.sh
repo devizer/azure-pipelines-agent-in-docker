dc3762f5d4f6045b0f41297d9f602cc0f4ee70d6
devizer <devizer@users.github.io>
2024-02-25 15:51:49 +0200

    16 Cloud init ***NO_CI***

--- cloud-init/Build.sh
+++ cloud-init/Build.sh
@@ -14,9 +14,10 @@ sudo virt-filesystems --all --long --uuid -h -a $file | sudo tee $key-LOGS/$key-
 sudo guestunmount $key-MNT >/dev/null 2>&1 || true
 set +e 
 for boot in sda1 sda2 sda3 sda4; do
-  export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
+  # export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
   echo ""; echo "TRY BOOT VOLUME $boot"
   sudo guestmount -a $file -m /dev/$boot $key-MNT
+  ls -la $key-MNT/boot |& tee $SYSTEM_ARTIFACTSDIRECTORY/$key-$boot-boot.files.txt
   sudo cp -f -r $key-MNT/boot/* $key-BOOTALL
   sudo cp -f -L $key-MNT/boot/{initrd.img,vmlinu?} $key-BOOT
   sudo guestunmount $key-MNT
