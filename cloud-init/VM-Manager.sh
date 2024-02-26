5c336965ef70ac747154f6e7626ab65830283534
devizer <devizer@users.github.io>
2024-02-26 15:12:03 +0200

    69 Cloud init (smoke test) ***NO_CI***

--- cloud-init/VM-Manager.sh
+++ cloud-init/VM-Manager.sh
@@ -141,7 +141,7 @@ runcmd:
 Say "cloud-localds verbose output"
 cloud-localds -v --disk-format qcow2 "$1" /tmp/cloud-config.txt
 echo "DONE: build schema"
-Ssy "VALIDATE CLOUD CONFIG"
+Say "VALIDATE CLOUD CONFIG"
 sudo cloud-init schema -c /tmp/cloud-config.txt || true
 echo "DONE: checkup schema"
 
