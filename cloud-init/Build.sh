34246bb75ad7d23615ae0206283bbc1b59030605
devizer <devizer@users.github.io>
2024-02-28 18:59:40 +0200

    Cloud Images, refactoring, postpone compress ***NO_CI***

--- cloud-init/Build.sh
+++ cloud-init/Build.sh
@@ -1,97 +1,10 @@
 set -ue; set -o pipefail
-XZ_COMPRESSION_LEVEL=9
-mkdir -p $SYSTEM_ARTIFACTSDIRECTORY/_logs
-sudo mkdir -p $THEWORKDIR/temp $THEWORKDIR/run; sudo chown -R $USER $THEWORKDIR; 
-work=$THEWORKDIR/temp
-pushd $work
-remotefile="$(basename "$IMAGEURL")"
-file=$KEY.img
-key=$KEY
-Say "Downloading raw image $KEY"
-echo "URL is $IMAGEURL"
-try-and-retry curl --connect-timeout 15 -ksfSL -o $file "$IMAGEURL" || rm -f $file
-if [[ "$IMAGEURL" == *".xz" ]]; then echo "Extracting $file.xz"; mv $file $file.xz; cat $file.xz | time xz -d > $file; rm -f $file.xz; fi
-ls -lah $file
-Say "Extracting kernel from /dev/sda1,2"
-mkdir -p $key-MNT $key-BOOTALL $key-BOOT $key-LOGS
-sudo virt-filesystems --all --long --uuid -h -a $file | sudo tee $key-LOGS/$key-filesystems.log | tee file-systems.txt
-# http://ask.xmodulo.com/mount-qcow2-disk-image-linux.html
-sudo guestunmount $key-MNT >/dev/null 2>&1 || true
-set +e 
-for boot in $(cat file-systems.txt | awk '$1 ~ /dev/ && $1 !~ /sda$/ {print $1}' | sort -u); do
-  # export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
-  echo ""; Say "TRY BOOT VOLUME $boot"
-  sudo guestmount -a $file -m $boot $key-MNT
-  echo The BOOT content
-  sudo ls -la $key-MNT/boot |& tee $SYSTEM_ARTIFACTSDIRECTORY/_logs/$key-$(basename $boot)-boot.files.txt
-  echo The ROOT content
-  sudo ls -la $key-MNT |& tee $SYSTEM_ARTIFACTSDIRECTORY/_logs/$key-$(basename $boot)-root.files.txt
-  sudo cp -f -r $key-MNT/boot/* $key-BOOTALL
-  # sudo cp -f -L $key-MNT/boot/{initrd.img,vmlinu?} $key-BOOT
-  # ubuntu without any boot volume
-  sudo bash -c "cp -f -L $key-MNT/boot/{initrd.img,vmlinu?} $key-BOOT"
-  # ubuntu with a boot volume
-  sudo bash -c "cp -f -L $key-MNT/{initrd.img,vmlinu?} $key-BOOT"
-  # debian
-  sudo bash -c "echo 'what about initrd.img?'; ls -lah $key-MNT/initrd.img-*; cp -f -L $key-MNT/initrd.img-* $key-BOOT/initrd.img"
-  sudo bash -c "echo 'what about vmlinuz?'; ls -lah $key-MNT/vmlinuz-*; cp -f -L $key-MNT/vmlinuz-* $key-BOOT/vmlinuz"
-  sudo bash -c "echo 'what about boot/initrd.img?'; ls -lah $key-MNT/boot/initrd.img-*; cp -f -L $key-MNT/boot/initrd.img-* $key-BOOT/initrd.img"
-  sudo bash -c "echo 'what about boot/vmlinuz?'; ls -lah $key-MNT/boot/vmlinuz-*; cp -f -L $key-MNT/boot/vmlinuz-* $key-BOOT/vmlinuz"
-  sudo guestunmount $key-MNT
-  mv $key-BOOT/vmlinux $key-BOOT/vmlinuz 2>/dev/null
-  sudo chown -R $USER $key-BOOT
-  if [[ -s $key-BOOT/initrd.img ]] && [[ -s $key-BOOT/vmlinuz ]]; then break; fi
-done
-set -e
-Say "Content of $key-BOOT"
-pushd $key-BOOT >/dev/null
-ls -lah *
-echo "Compressed Content of $key-BOOT"
-sudo xz -z -3 *
-ls -lah *
-popd >/dev/null
-sudo chown -R $USER $key-BOOT
+source VM-Manager.sh 
+Prepare-VM-Image "$IMAGEURL" $THEWORKDIR/run 16G
 
-Say "Resizing image"
-qemu-img create -f qcow2 disk.intermediate.compacting.qcow2 15G
-sudo virt-resize --expand /dev/sda1 $file disk.intermediate.compacting.qcow2
-qemu-img convert -O qcow2 disk.intermediate.compacting.qcow2 $key.qcow2
-rm -f disk.intermediate.compacting.qcow2
-sudo virt-filesystems --all --long --uuid -h -a $key.qcow2 | sudo tee $key-LOGS/$key-filesystems.resized.log | tee filesystems.resized.log
-root_partition_index=$(cat filesystems.resized.log | awk '$4 ~ /cloudimg-rootfs/ {print $1}' | sed 's/\/dev\/sda//' | sort -u)
-if [[ "${root_partition_index:-}" == "" ]]; then 
-  # debian
-  root_partition_index=$(cat filesystems.resized.log | awk '$3 ~ /ext4/ {print $1}' | sed 's/\/dev\/sda//' | sort -u)
-fi
-printf $root_partition_index > $SYSTEM_ARTIFACTSDIRECTORY/root.partition.index.txt
-echo "QCOW2 Size ($key.qcow2)"
-ls -lah $key.qcow2
-XZ_COMPRESSION_LEVEL=${XZ_COMPRESSION_LEVEL:-9}
-Say "Compressing ($key.qcow2), LEVEL $XZ_COMPRESSION_LEVEL"
-echo "CPU: $(Get-CpuName)"
-# cat $key.qcow2 | xz -z -9 -e > $SYSTEM_ARTIFACTSDIRECTORY/disk.qcow2.xz
-time 7z a -txz -mx=$XZ_COMPRESSION_LEVEL -mmt=$(nproc) $SYSTEM_ARTIFACTSDIRECTORY/disk.qcow2.xz $key.qcow2
-
-ls -lah $SYSTEM_ARTIFACTSDIRECTORY/disk.qcow2.xz
-
-sudo cp -a $key-BOOT/. $SYSTEM_ARTIFACTSDIRECTORY
-# sudo cp -a $key-BOOTALL $SYSTEM_ARTIFACTSDIRECTORY
-sudo cp -a $key-LOGS/. $SYSTEM_ARTIFACTSDIRECTORY/_logs
-sudo chown -R $USER $SYSTEM_ARTIFACTSDIRECTORY
-
-Say "Complete ($key.qcow2)"
-
-rundir=$THEWORKDIR/run
-cat $key-BOOT/initrd.img.xz | xz -d > $rundir/initrd.img
-cat $key-BOOT/vmlinuz.xz | xz -d > $rundir/vmlinuz
-pushd $rundir
-cat $SYSTEM_ARTIFACTSDIRECTORY/disk.qcow2.xz | xz -d > disk.qcow2
-cp $SYSTEM_ARTIFACTSDIRECTORY/root.partition.index.txt root.partition.index.txt
-(ls -lah; echo "root.partition.index is $(cat root.partition.index.txt)";) |& tee $SYSTEM_ARTIFACTSDIRECTORY/_logs/final.artifacts.txt
-popd
-
-popd # $work
+Say "Copy to $THEWORKDIR/run to $SYSTEM_ARTIFACTSDIRECTORY"
+# later we will compress it
+cp -a -f -v $THEWORKDIR/run $SYSTEM_ARTIFACTSDIRECTORY
 
 Say "VM-Launcher-Smoke-Test in [$(pwd)]"
-source VM-Manager.sh 
 VM-Launcher-Smoke-Test
--- cloud-init/VM-Manager.sh
+++ cloud-init/VM-Manager.sh
@@ -1,7 +1,76 @@
-set -o pipefail
+set -o pipefail; set -eu
 
+# 1) Download, 2) Extract initrd.img and vmlinuz 3) Resize up to 4) Move to $to_folder as disk.qcow2
 function Prepare-VM-Image() {
-  echo "todo: download and expand image and kernel to specified folder"
+  local url="$1"
+  local to_folder="$2"
+  local size="${3:-16G}"
+
+  sudo mkdir -p "${to_folder}"/temp "${to_folder}"/_logs; sudo chown -R $USER "${to_folder}"; 
+  rm -rf "${to_folder}"/temp/* || true
+  work="${to_folder}"/temp
+  pushd $work >/dev/null
+  file=original.img
+  key=original
+  Say "Downloading raw cloud image as $(pwd)/$file"
+  echo "URL is $url"
+  try-and-retry curl --connect-timeout 30 -ksfSL -o $file "$url" || rm -f $file
+  if [[ "$url" == *".xz" ]]; then echo "Extracting $file.xz"; mv $file $file.xz; cat $file.xz | time xz -d > $file; rm -f $file.xz; fi
+  ls -lah $file
+  Say "Extracting kernel from /dev/sda1,2,..."
+  mkdir -p $key-MNT $key-BOOTALL $key-BOOT $key-LOGS
+  sudo virt-filesystems --all --long --uuid -h -a $file | tee "${to_folder}"/_logs/filesystems.txt
+  # http://ask.xmodulo.com/mount-qcow2-disk-image-linux.html
+  sudo guestunmount $key-MNT >/dev/null 2>&1 || true
+  set +e 
+  for boot in $(cat "${to_folder}"/_logs/filesystems.txt | awk '$1 ~ /dev/ && $1 !~ /sda$/ {print $1}' | sort -u); do
+    # export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
+    echo ""; Say "TRY BOOT VOLUME $boot"
+    sudo guestmount -a $file -m $boot $key-MNT
+    echo The BOOT content
+    sudo ls -la $key-MNT/boot |& tee "${to_folder}"/_logs/$(basename $boot)-boot.files.txt
+    echo The ROOT content
+    sudo ls -la $key-MNT |& tee "${to_folder}"/_logs/$(basename $boot)-root.files.txt
+    sudo cp -f -r $key-MNT/boot/* $key-BOOTALL
+    # sudo cp -f -L $key-MNT/boot/{initrd.img,vmlinu?} $key-BOOT
+    # ubuntu without any boot volume
+    sudo bash -c "cp -f -L $key-MNT/boot/{initrd.img,vmlinu?} $key-BOOT"
+    # ubuntu with a boot volume
+    sudo bash -c "cp -f -L $key-MNT/{initrd.img,vmlinu?} $key-BOOT"
+    # debian
+    sudo bash -c "echo 'what about initrd.img?'; ls -lah $key-MNT/initrd.img-*; cp -f -L $key-MNT/initrd.img-* $key-BOOT/initrd.img"
+    sudo bash -c "echo 'what about vmlinuz?'; ls -lah $key-MNT/vmlinuz-*; cp -f -L $key-MNT/vmlinuz-* $key-BOOT/vmlinuz"
+    sudo bash -c "echo 'what about boot/initrd.img?'; ls -lah $key-MNT/boot/initrd.img-*; cp -f -L $key-MNT/boot/initrd.img-* $key-BOOT/initrd.img"
+    sudo bash -c "echo 'what about boot/vmlinuz?'; ls -lah $key-MNT/boot/vmlinuz-*; cp -f -L $key-MNT/boot/vmlinuz-* $key-BOOT/vmlinuz"
+    sudo guestunmount $key-MNT
+    mv $key-BOOT/vmlinux $key-BOOT/vmlinuz 2>/dev/null
+    sudo chown -R $USER $key-BOOT
+    if [[ -s $key-BOOT/initrd.img ]] && [[ -s $key-BOOT/vmlinuz ]]; then break; fi
+  done
+  set -e
+  Say "Final Content of Original BOOT"
+  sudo chown -R $USER $key-BOOT
+  ls -lah $key-BOOT
+
+  Say "Resizing image up to $size"
+  qemu-img create -f qcow2 disk.intermediate.compacting.qcow2 $size
+  sudo virt-resize --expand /dev/sda1 $file disk.intermediate.compacting.qcow2
+  # qemu-img convert -O qcow2 disk.intermediate.compacting.qcow2 $key.qcow2
+  mv disk.intermediate.compacting.qcow2 $key.qcow2 # faster!
+  rm -f disk.intermediate.compacting.qcow2
+  sudo virt-filesystems --all --long --uuid -h -a $key.qcow2 | tee "${to_folder}"/_logs/filesystems.resized.txt
+  root_partition_index=$(cat "${to_folder}"/_logs/filesystems.resized.txt | awk '$4 ~ /cloudimg-rootfs/ {print $1}' | sed 's/\/dev\/sda//' | sort -u)
+  if [[ "${root_partition_index:-}" == "" ]]; then 
+    # debian
+    root_partition_index=$(cat "${to_folder}"/_logs/filesystems.resized.txt | awk '$3 ~ /ext4/ {print $1}' | sed 's/\/dev\/sda//' | sort -u)
+  fi
+  mv $key.qcow2 "${to_folder}"/disk.qcow2
+  mv $key-BOOT/* "${to_folder}"
+  popd >/dev/null
+  sudo rm -rf "${to_folder}"/temp
+  echo "Completed: " $(ls "${to_folder}" | sort)
+  Say "DONE. Root Partition Index is ${root_partition_index:-UNKNOWN}"
+  printf $root_partition_index > "${to_folder}"/root.partition.index.txt
 }
 
 function Build-Cloud-Config() {
@@ -82,6 +151,8 @@ bootcmd:
            . /etc/os-release
            if [ -z "$VERSION_CODENAME" ]; then VERSION_CODENAME=$DISTRIB_CODENAME; fi
            if [ -z "$VERSION_CODENAME" ]; then VERSION_CODENAME=$VERSION_ID; fi
+           if [ "$VERSION_CODENAME" = "14.04" ]; then VERSION_CODENAME=trusty; fi
+           if [ "$VERSION_CODENAME" = "16.04" ]; then VERSION_CODENAME=xenial; fi
         else
            ID=centos
            VERSION_CODENAME=6
