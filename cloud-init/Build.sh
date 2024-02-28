b959ab1e13db69ac3aad821eac51cb7175cb627e
devizer <devizer@users.github.io>
2024-02-28 19:23:04 +0200

    4 Cloud Images, refactoring, postpone compress ***NO_CI***

--- cloud-init/Build.sh
+++ cloud-init/Build.sh
@@ -4,7 +4,9 @@ Prepare-VM-Image "$IMAGEURL" $THEWORKDIR/run 16G
 
 Say "Copy to $THEWORKDIR/run to $SYSTEM_ARTIFACTSDIRECTORY"
 # later we will compress it
-cp -a -f -v $THEWORKDIR/run $SYSTEM_ARTIFACTSDIRECTORY
+pushd $THEWORKDIR/run
+cp -a -f -v . $SYSTEM_ARTIFACTSDIRECTORY
+popd
 
 Say "VM-Launcher-Smoke-Test in [$(pwd)]"
 VM-Launcher-Smoke-Test
