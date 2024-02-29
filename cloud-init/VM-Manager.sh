77eea46a297f27402e310c3ed244ba2a8d17cc88
devizer <devizer@users.github.io>
2024-02-29 13:57:03 +0200

    Cloud Init: timeout in seconds ***NO_CI***

--- cloud-init/VM-Manager.sh
+++ cloud-init/VM-Manager.sh
@@ -257,19 +257,25 @@ function File-To-Base64() { cat "$1" | base64 -w 0; }
 function Wait-For-VM() {
   local lauch_options="$1"
 
-  local n=150
-  while [ $n -gt 0 ]; do
-    echo "$n) Waiting for ssh connection to VM on port $VM_SSH_PORT."
+  local n=0
+  local startAt="$(get_global_seconds)"
+  while [ 1 -eq 1 ]; do
+    local current="$(get_global_seconds)"
+    current="$((current-startAt))"
+    current="$((15*60 - current))"
+    if [[ $current -ge 0 ]]; then break; fi
+    echo "{$current #$n} Waiting for ssh connection to VM on port $VM_SSH_PORT."
     set +e
     sshpass -p "p1ssw0rd" ssh -o StrictHostKeyChecking=no "root@127.0.0.1" -p "${VM_SSH_PORT}" "sh -c 'echo; echo WELCOME TO VM; uname -a; uptime'" 2>/dev/null
     local ok=$?;
     set -e
     if [ $ok -eq 0 ]; then break; fi
     sleep 5
-    n=$((n-1))
+    n=$((n+1))
   done
   if [ $ok -ne 0 ]; then
-    echo "vm build agent ERROR: VM is not responding via ssh"
+    echo "VM build agent ERROR: VM is not responding via ssh"
+    Say --Display-As=Error "VM build agent ERROR: VM is not responding via ssh"
     return 1;
   fi
   local mapto="$lauch_options/fs"
@@ -341,6 +347,31 @@ function Wait-For-VM() {
     tar xf /tmp/outcome.tar
   popd
 }
+
+# Copy from Say
+function get_global_seconds() {
+  theSYSTEM="${theSYSTEM:-$(uname -s)}"
+  if [[ ${theSYSTEM} != "Darwin" ]]; then
+      # uptime=$(</proc/uptime);                                # 42645.93 240538.58
+      uptime="$(cat /proc/uptime 2>/dev/null)";                 # 42645.93 240538.58
+      if [[ -z "${uptime:-}" ]]; then
+        # secured, use number of seconds since 1970
+        echo "$(date +%s)"
+        return
+      fi
+      IFS=' ' read -ra uptime <<< "$uptime";                    # 42645.93 240538.58
+      uptime="${uptime[0]}";                                    # 42645.93
+      uptime=$(LC_ALL=C LC_NUMERIC=C printf "%.0f\n" "$uptime") # 42645
+      echo $uptime
+  else 
+      # https://stackoverflow.com/questions/15329443/proc-uptime-in-mac-os-x
+      boottime=`sysctl -n kern.boottime | awk '{print $4}' | sed 's/,//g'`
+      unixtime=`date +%s`
+      timeAgo=$(($unixtime - $boottime))
+      echo $timeAgo
+  fi
+}
+
 
 function VM-Launcher-Smoke-Test() {
   mkdir -p /tmp/cloud-init-smoke-test-provisia
