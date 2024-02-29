ffed599b1c1f51df8bdcb76e831aaeed883aab34
devizer <devizer@users.github.io>
2024-02-29 15:10:50 +0200

    7 Cloud Init: try-and-retry mount boot, rpi-os 10.zip and 12.xz, elapsed  ***NO_CI***

--- cloud-init/VM-Manager.sh
+++ cloud-init/VM-Manager.sh
@@ -15,8 +15,11 @@ function Prepare-VM-Image() {
   Say "Downloading raw cloud image as $(pwd)/$file"
   echo "URL is $url"
   try-and-retry curl --connect-timeout 30 -ksfSL -o $file "$url" || rm -f $file
+  # rpi.img.xz -> rpi.img
   if [[ "$url" == *".xz" ]]; then echo "Extracting $file.xz"; mv $file $file.xz; cat $file.xz | time xz -d > $file; rm -f $file.xz; fi
+  # rpi.zip -> rpi.img
   if [[ "$url" == *".zip" ]]; then echo "Extracting $file.zip"; mv $file $file.zip; 7z x $file.zip; rm -f $file.zip; fi
+  ls -lah; echo ""
   ls -lah $file
   Say "Extracting kernel from /dev/sda1,2,..."
   mkdir -p $key-MNT $key-BOOTALL $key-BOOT $key-LOGS
