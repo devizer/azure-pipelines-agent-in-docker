670db9f4a46591fbcc4d383ebf491328a02c9e59
devizer <devizer@users.github.io>
2024-03-01 11:45:54 +0200

    5 Cloud Init: tests v4.7.2  ***NO_CI***

--- cloud-init/Build-Step.sh
+++ cloud-init/Build-Step.sh
@@ -3,7 +3,7 @@ source VM-Manager.sh
 
 Prepare-VM-Image "$IMAGEURL" $THEWORKDIR/run 16G
 
-Say "Copy to $THEWORKDIR/run to $SYSTEM_ARTIFACTSDIRECTORY"
+Say "Copy VM ($THEWORKDIR/run) to Artifacts ($SYSTEM_ARTIFACTSDIRECTORY)"
 # later we will compress it
 pushd $THEWORKDIR/run
 cp -a -f -v . $SYSTEM_ARTIFACTSDIRECTORY
