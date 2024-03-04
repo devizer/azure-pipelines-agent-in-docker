a49872b8090a0ce410b73068fbfa581aa6e36094
devizer <devizer@users.github.io>
2024-03-04 08:29:04 +0200

    15 Cloud init: faster if resize skipped, versions in logs, expired gpg, GAWK, scsi-hd, debian 8+9+10+11+12 armhf (skip resize) and 2nd debconf-get-selections, packages.txt in outcome ***NO_CI***

--- cloud-init/VM-Manager.sh
+++ cloud-init/VM-Manager.sh
@@ -441,9 +441,9 @@ mkdir -p /root/_logs
 
 Say "APT UPDATE"
 echo "Invloke apt-get update"
-time (apt-get --allow-releaseinfo-change update -qq || apt-get update -qq)
+time (apt-get --allow-releaseinfo-change update || apt-get update )
 echo "Invloke apt-get install"
-time (apt-get install -y --force-yes -qq debconf-utils jq gawk) # missing on old distros
+time (apt-get install -y --force-yes debconf-utils jq gawk) # missing on old distros
 
 Say "Grab debconf-get-selections"
 debconf-get-selections --installer |& tee /root/_logs/debconf-get-selections.part1.txt || true
