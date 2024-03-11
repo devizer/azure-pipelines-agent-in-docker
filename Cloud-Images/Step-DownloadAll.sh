89ecd3e00391e70c2a24b9be70722db83a2f00d6
devizer <devizer@users.github.io>
2024-03-11 15:04:41 +0200

    7th Test Cloud Images: Download All ***NO_CI***

--- Cloud-Images/Step-DownloadAll.sh
+++ Cloud-Images/Step-DownloadAll.sh
@@ -1,15 +1,18 @@
 set -eu; set -o pipefail
 Say --Reset-Stopwatch
+bash Download-CloudImage.sh "$image" "$toFolder"
 
 images='
+  armhf-debian-8       armhf-debian-9       armhf-debian-10      armhf-debian-11     
+  armhf-debian-12
+  arm64-debian-10      arm64-debian-11      arm64-debian-12      arm64-debian-13
+
   armhf-ubuntu-14.04   armhf-ubuntu-16.04   armhf-ubuntu-18.04   armhf-ubuntu-20.04  
   armhf-ubuntu-22.04   armhf-ubuntu-23.10   armhf-ubuntu-24.04                       
   arm64-ubuntu-14.04   arm64-ubuntu-16.04   arm64-ubuntu-18.04   arm64-ubuntu-20.04  
   arm64-ubuntu-22.04   arm64-ubuntu-23.10   arm64-ubuntu-24.04                       
-  armhf-debian-8       armhf-debian-9       armhf-debian-10      armhf-debian-11     
-  armhf-debian-12
-  arm64-debian-10      arm64-debian-11      arm64-debian-12      arm64-debian-13
 '
+
 
 sudo chown -R $USER "$THEWORKDIR"
 n=0
