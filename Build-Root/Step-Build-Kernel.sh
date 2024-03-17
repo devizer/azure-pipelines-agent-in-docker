d1ec75d49b0767e519596e4ed40fde9e89df355e
devizer <devizer@users.github.io>
2024-03-17 10:14:19 +0200

    4 Build Kernel ***NO_CI***

--- Build-Root/Step-Build-Kernel.sh
+++ Build-Root/Step-Build-Kernel.sh
@@ -1,7 +1,8 @@
 set -eu; set -o pipefail
 source install-linaro-arm32.sh 
 Say "Apt install"
-sudo apt install bc build-essential gcc-aarch64-linux-gnu gcc-arm-linux-gnueabihf gcc-arm-linux-gnueabi git unzip libncurses5-dev bison flex libssl-dev | grep "Setting\|Unpack"
+sudo apt-get update -qq
+sudo apt-get install bc build-essential gcc-aarch64-linux-gnu gcc-arm-linux-gnueabihf gcc-arm-linux-gnueabi git unzip libncurses5-dev bison flex libssl-dev | grep "Setting\|Unpack"
 
 echo '
 https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.19.310.tar.xz
