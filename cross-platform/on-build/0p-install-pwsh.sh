8a82b6dbd65931d31b7535006e92c90a059dc999
devizer <devizer@users.github.io>
2024-04-11 09:29:37 +0300

    4th pwsh checkup ssl, Fix curl on debian12 and ubuntu22+ ***NO_CI***

--- cross-platform/on-build/0p-install-pwsh.sh
+++ cross-platform/on-build/0p-install-pwsh.sh
@@ -3,6 +3,14 @@
 Say "Installing PowerShell";
 script=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-and-nodejs.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash -s pwsh;
 
+if [[ -n "$(command -v ldconfig)" ]] && [[ -z "$(ldconfig -p | grep libssl.so.1.1)" ]]; then
+  Say "openssl 1.1 missing. Installing precompiled 1.1.1m"
+  url=https://raw.githubusercontent.com/devizer/glist/master/install-libssl-1.1.sh; 
+  (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) | bash
+else
+  echo "Using preinstalled libssl.so.1.1"
+fi
+
 Say "Checking Up SSL over PowerShell";
 pwsh -c "Invoke-WebRequest https://google.com"
 
