ef7cfc25c1189c1684cbad6650929a8c83dcfa9b
devizer <devizer@users.github.io>
2024-09-13 02:03:32 +0300

    Cross Platform Pipeline ***NO_CI***

--- Cross-Platform-Pipeline/trigger-local-build.sh
+++ Cross-Platform-Pipeline/trigger-local-build.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/devizer/azure-pipelines-agent-in-docker $HOME/azure-pipelines-agent-in-docker; cd $HOME/azure-pipelines-agent-in-docker; git pull; cd Cross-Platform-Pipeline; bash -e test-local-build.sh
+git clone https://github.com/devizer/azure-pipelines-agent-in-docker $HOME/azure-pipelines-agent-in-docker; cd $HOME/azure-pipelines-agent-in-docker; git pull; cd Cross-Platform-Pipeline; bash -e local-build.sh
