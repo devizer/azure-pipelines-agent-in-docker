3dec88d7c62cc8f182005cc0bbee92829fc421b1
devizer <devizer@users.github.io>
2024-09-13 03:52:56 +0300

    Cross Platform Pipeline ***NO_CI***

--- Cross-Platform-Pipeline/local-build.sh
+++ Cross-Platform-Pipeline/local-build.sh
@@ -1,7 +1,8 @@
 cd Docker-Image-Builder 
 export BASE_IMAGE="${BASE_IMAGE:-ubuntu:24.04}"
-export QEMU_IMAGE_ID="${QEMU_IMAGE_ID:-arm64-debian-10}"
+export QEMU_IMAGE_ID="${QEMU_IMAGE_ID:-armel-debian-11}"
 time docker build \
+  --progress plain \
   --build-arg BASE_IMAGE="${BASE_IMAGE}" \
   --build-arg QEMU_IMAGE_ID="${QEMU_IMAGE_ID}" \
   -t devizervlad/crossplatform-pipeline:${QEMU_IMAGE_ID} . 
