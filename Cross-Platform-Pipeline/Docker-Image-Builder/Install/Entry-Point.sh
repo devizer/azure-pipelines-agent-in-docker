8f5d5be29ee980615bc64b403887da65b6626717
devizer <devizer@users.github.io>
2024-09-13 08:52:40 +0300

    Cross Platform Pipeline ***NO_CI***

--- Cross-Platform-Pipeline/Docker-Image-Builder/Install/Entry-Point.sh
+++ Cross-Platform-Pipeline/Docker-Image-Builder/Install/Entry-Point.sh
@@ -10,4 +10,24 @@ Say "LAUNCH-VM in $(pwd)"
 Launch-VM $arch /Cloud-Image/cloud-config.qcow2 /Cloud-Image
 pid=$(cat /Cloud-Image/pid)
 Say "VM LAUNCHED. PID is $pid"
+
+export VM_PROVISIA_FOLDER=/app
+export HOST_OUTCOME_FOLDER=/app
+mkdir -p /app
+echo "For VM Content" > /app/my-source.txt
+
+pushd /app
+popd
+
+echo '
+VM_SSH_PORT='$VM_SSH_PORT'
+VM_PROVISIA_FOLDER='"'"$VM_PROVISIA_FOLDER"'"'
+VM_VARIABLES='"'"${VM_VARIABLES:-}"'"'
+VM_USER_NAME='"'"${VM_USER_NAME:-root}"'"'
+VM_PREBOOT_SCRIPT='"'"${VM_PREBOOT_SCRIPT:-}"'"'
+VM_POSTBOOT_SCRIPT='"'""${VM_POSTBOOT_SCRIPT:-}""'"'
+VM_POSTBOOT_ROLE='"'"${VM_POSTBOOT_ROLE:-root}"'"'
+VM_OUTCOME_FOLDER='"'"${VM_OUTCOME_FOLDER:-/root}"'"'
+' > "/Cloud-Image/variables"
+
 Wait-For-VM /Cloud-Image
--- Cross-Platform-Pipeline/local-build.sh
+++ Cross-Platform-Pipeline/local-build.sh
@@ -5,6 +5,7 @@ cd Docker-Image-Builder
 export BASE_IMAGE="${BASE_IMAGE:-ubuntu:20.04}"
 export QEMU_IMAGE_ID="${QEMU_IMAGE_ID:-armel-debian-8}"
 docker rm -f qemu-vm || true
+docker rm -f qemu-vm-host || true
 docker rmi -f devizervlad/crossplatform-pipeline:${QEMU_IMAGE_ID} || true
 time docker build \
   --progress plain \
@@ -16,5 +17,5 @@ Say "Crossplatform Pipeline Images"
 docker image ls | grep "devizervlad/crossplatform-pipeline"
 
 # https://stackoverflow.com/a/49021109 (fuse in container)
-docker run --name qemu-vm --hostname qemu-vm --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor:unconfined -it devizervlad/crossplatform-pipeline:${QEMU_IMAGE_ID} bash -c "uname -a; free -m; df -h -T"
+docker run --name qemu-vm-host --hostname qemu-vm-host --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor:unconfined -it devizervlad/crossplatform-pipeline:${QEMU_IMAGE_ID} bash -c "uname -a; free -m; df -h -T"
 
