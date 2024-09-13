d4170ceb48b01fc266f6a7f5cb17e0cd6691d68e
devizer <devizer@users.github.io>
2024-09-13 12:47:16 +0300

    Cross Platform Pipeline Images ***NO_CI***

--- Cross-Platform-Pipeline/local-build.sh
+++ Cross-Platform-Pipeline/local-build.sh
@@ -4,9 +4,9 @@ chmod +x ./Docker-Image-Builder/Install/*.sh
 cd Docker-Image-Builder 
 export BASE_IMAGE="${BASE_IMAGE:-ubuntu:22.04}"
 export QEMU_IMAGE_ID="${QEMU_IMAGE_ID:-armhf-debian-12}"
-docker rm -f qemu-vm || true
-docker rm -f qemu-vm-host || true
-docker rmi -f devizervlad/crossplatform-pipeline:${QEMU_IMAGE_ID} || true
+docker rm -f qemu-vm-container 2>/dev/null || true
+docker rm -f qemu-vm-host 2>/dev/null || true
+docker rmi -f devizervlad/crossplatform-pipeline:${QEMU_IMAGE_ID} 2>/dev/null || true
 time docker build \
   --progress plain \
   --build-arg BASE_IMAGE="${BASE_IMAGE}" \
@@ -16,6 +16,6 @@ time docker build \
 Say "Crossplatform Pipeline Images"
 docker image ls | grep "devizervlad/crossplatform-pipeline"
 
+Say "Smoketest of newly created image for [$QEMU_IMAGE_ID]"
 # https://stackoverflow.com/a/49021109 (fuse in container)
-docker run --privileged --name qemu-vm-host --hostname qemu-vm-host --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor:unconfined -t devizervlad/crossplatform-pipeline:${QEMU_IMAGE_ID} bash -c "uname -a; Say \"FOLDER IS [\$(pwd)]\"; free -m; df -h -T; uname -a"
-
+docker run --privileged --name qemu-vm-container --hostname qemu-vm-container --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor:unconfined -t devizervlad/crossplatform-pipeline:${QEMU_IMAGE_ID} bash -c "uname -a; Say \"FOLDER IS [\$(pwd)]\"; free -m; df -h -T; uname -a"
