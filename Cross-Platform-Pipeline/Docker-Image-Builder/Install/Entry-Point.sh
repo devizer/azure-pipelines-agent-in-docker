8a34c5d80158158bcb2539c097c7e5210af5a1bf
devizer <devizer@users.github.io>
2024-09-13 09:15:38 +0300

    Cross Platform Pipeline ***NO_CI***

--- Cross-Platform-Pipeline/Docker-Image-Builder/Install/Entry-Point.sh
+++ Cross-Platform-Pipeline/Docker-Image-Builder/Install/Entry-Point.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo ">>> ENTRY-POINT <<<"
 set -eu; set -o pipefail;
+Say --Reset-Stopwatch
 source /Install/VM-Manager.sh
 arch="$(cat /Cloud-Image/arch.txt)"
 export VM_SSH_PORT=22022
 export VM_CPUS="$(nproc)" 
 export VM_MEM="2048M"
-Say "LAUNCH-VM in $(pwd)"
+Say "LAUNCH-VM [QEMU_IMAGE_ID]"
 Launch-VM $arch /Cloud-Image/cloud-config.qcow2 /Cloud-Image
 pid=$(cat /Cloud-Image/pid)
 Say "VM LAUNCHED. PID is $pid"
