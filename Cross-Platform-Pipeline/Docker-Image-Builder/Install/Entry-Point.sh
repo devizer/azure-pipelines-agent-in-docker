75e38416f80661019eeccd2efd1726faec008410
devizer <devizer@users.github.io>
2024-09-13 19:23:53 +0300

    Cross Platform Pipeline Images ***NO_CI***

--- Cross-Platform-Pipeline/Docker-Image-Builder/Install/Entry-Point.sh
+++ Cross-Platform-Pipeline/Docker-Image-Builder/Install/Entry-Point.sh
@@ -15,6 +15,7 @@ Say "VM LAUNCHED. PID is $pid"
 
 export VM_PROVISIA_FOLDER=/job
 export HOST_OUTCOME_FOLDER="${VM_PROVISIA_FOLDER}"
+export VM_OUTCOME_FOLDER="${VM_PROVISIA_FOLDER}"
 export VM_POSTBOOT_SCRIPT="$@"
 mkdir -p "${VM_PROVISIA_FOLDER}"
 echo "For VM Content" > "${VM_PROVISIA_FOLDER}"/my-source.txt
@@ -32,7 +33,7 @@ VM_USER_NAME='"'"${VM_USER_NAME:-root}"'"'
 VM_PREBOOT_SCRIPT='"'"${VM_PREBOOT_SCRIPT:-}"'"'
 VM_POSTBOOT_SCRIPT='"'""${VM_POSTBOOT_SCRIPT:-}""'"'
 VM_POSTBOOT_ROLE='"'"${VM_POSTBOOT_ROLE:-root}"'"'
-VM_OUTCOME_FOLDER='"'"${VM_OUTCOME_FOLDER:-/root}"'"'
+VM_OUTCOME_FOLDER='"'"${VM_OUTCOME_FOLDER:-/job}"'"'
 ' > "/Cloud-Image/variables"
 
 Wait-For-VM /Cloud-Image
