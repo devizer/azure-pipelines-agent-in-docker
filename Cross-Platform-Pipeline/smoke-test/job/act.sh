958fddafd252380b8fb173deace32ea1672d8c67
devizer <devizer@users.github.io>
2024-09-15 09:24:36 +0300

    12 x86/x64 pipeline images: Cross Platform Pipeline ***NO_CI***

--- Cross-Platform-Pipeline/smoke-test/job/act.sh
+++ Cross-Platform-Pipeline/smoke-test/job/act.sh
@@ -1,6 +1,13 @@
 set -eu; set -o pipefail
 Say "Perform smoke test job"
-(echo "HOST: $(hostname)"; echo "KERNEL: $(uname -r)"; printf "\nMEMORY\n"; Drop-FS-Cache; free -m; printf "\nSTORAGE\n"; df -h -T; echo ""; url=https://raw.githubusercontent.com/devizer/glist/master/Benchmark-net40.exe; try-and-retry curl -ksfSL -o /tmp/Benchmark-net40.exe "$url"; time mono /tmp/Benchmark-net40.exe; printf "\nOS RELEASE\n"; cat /etc/os-release; cat /etc/debian_version 2>/dev/null) | tee results.txt
+(
+  echo "HOST: $(hostname)"; echo "KERNEL: $(uname -r)"; 
+  printf "\nMEMORY\n"; Drop-FS-Cache; free -m; 
+  printf "\nSTORAGE\n"; df -h -T; 
+  echo ""; url=https://raw.githubusercontent.com/devizer/glist/master/Benchmark-net40.exe; try-and-retry curl -ksfSL -o /tmp/Benchmark-net40.exe "$url"; time mono /tmp/Benchmark-net40.exe; 
+  printf "\nOS RELEASE\n"; cat /etc/os-release; cat /etc/debian_version 2>/dev/null
+) | tee results.txt
+
 echo $(uname -r) > kernel.txt
 
 rm -rf /var/lib/apt/* /var/cacheb/apt/* || true
