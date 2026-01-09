4b2799a3da42a59053b3ec1d5577464bb6273f0c
devizer <devizer@github.io>
2026-01-09 07:33:17 +0200

    19th DevOps Lib Tests [skip ci] ***NO_CI***

--- DevOps-Library/Test-On-Bash-Matrix.sh
+++ DevOps-Library/Test-On-Bash-Matrix.sh
@@ -2,5 +2,5 @@
 for img in 3.2 4.0.44 5.0 latest; do
   Colorize Green "TEST ON BASH [$img]"
   RetryOnFail docker pull -q bash:"$img"
-  docker run -t bash:"$img" bash -c 'echo BASH VERSION is [$BASH_VERSION]'
+  docker run -v $PWD:/app -t bash:"$img" bash -c 'echo BASH VERSION is [$BASH_VERSION]; cd /app; set -e; bash "[Test] ALL.sh"; bash "[Test] Fetch S5 Dashboard API.sh"'
 done
