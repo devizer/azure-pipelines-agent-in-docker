0bd0bdee49653962ccd4de463a6dff0b999c932b
devizer <devizer@github.io>
2026-01-09 08:05:50 +0200

    23th DevOps Lib Tests [skip ci] ***NO_CI***

--- DevOps-Library/Test-On-Bash-Matrix.sh
+++ DevOps-Library/Test-On-Bash-Matrix.sh
@@ -4,6 +4,8 @@ for img in 3.2 4.0.44 4.1 4.2 4.3 4.4 5.0 5.1 5.2 5.3 latest; do
   RetryOnFail docker pull -q bash:"$img"
   docker run -v $PWD:/app -t bash:"$img" bash -c 'echo BASH VERSION is [$BASH_VERSION]; cd /app; 
     . DevOps-Library.sh; 
+    RetryOnFail apk update;
+    RetryOnFail apk add curl;
     for s in "[Test] Validate File Is Not Empty.sh" "[Test] Wait For Http.sh" "[Test] Download.sh" "[Test] Retry.sh" "[Test] ALL.sh" "[Test] Fetch S5 Dashboard API.sh"; do
       Colorize Cyan $s;
       bash "$s"
