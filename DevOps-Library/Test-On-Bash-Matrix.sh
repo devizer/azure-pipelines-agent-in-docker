3a20e7d08e53e7d16ea5ce0bc162230e804f9ca0
devizer <devizer@github.io>
2026-01-09 07:01:34 +0200

    7th DevOps Lib Tests [skip ci] ***NO_CI***

---
+++ DevOps-Library/Test-On-Bash-Matrix.sh
@@ -0,0 +1,6 @@
+. DevOps-Library.sh
+for img in 3.2 4.0.44 5.0 latest; do
+  Colorize Green "TEST ON BASH [$img]"
+  RetryOnFail docker pull bash:"img"
+  docker run -t bash:"img" 'echo BASH VERSION is [$BASH_VERSION]'
+done
