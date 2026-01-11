15fb7ae65e580491ff86fd2c35d2013dce393db9
devizer <devizer@github.io>
2026-01-11 05:14:03 +0200

    17th Test-DevOps-Library.yml: [Test] Run-Remote-Script.sh [skip ci] ***NO_CI***

--- DevOps-Library/Test-On-Bash-Matrix.sh
+++ DevOps-Library/Test-On-Bash-Matrix.sh
@@ -1,11 +1,11 @@
 . DevOps-Library.sh
 for img in 3.2 4.0.44 4.1 4.2 4.3 4.4 5.0 5.1 5.2 5.3 latest; do
   Colorize Green "TEST ON BASH [$img]"
-  RetryOnFail docker pull -q bash:"$img"
+  Retry-On-Fail docker pull -q bash:"$img"
   docker run -v $PWD:/app -t bash:"$img" bash -c 'echo BASH VERSION is [$BASH_VERSION]; cd /app; 
     . DevOps-Library.sh; 
-    RetryOnFail apk update;
-    RetryOnFail apk add curl;
+    Retry-On-Fail apk update;
+    Retry-On-Fail apk add curl;
     for s in "[Test] Validate File Is Not Empty.sh" "[Test] Wait For Http.sh" "[Test] Download.sh" "[Test] Retry.sh" "[Test] ALL.sh" "[Test] Fetch S5 Dashboard API.sh"; do
       Colorize Cyan $s;
       bash "$s"
