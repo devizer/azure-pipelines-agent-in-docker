13d71951136145d7fe4ae0ee5b2403ac4bdad920
devizer <devizer@github.io>
2026-01-16 13:56:09 +0200

    Build, Test-Windows-Docker-Images.yml [skip ci]

--- Windows-Docker-Images/Setup-Container.ps1
+++ Windows-Docker-Images/Setup-Container.ps1
@@ -7,7 +7,7 @@ foreach($attempt in 1..3) { try { iex ((New-Object System.Net.WebClient).Downloa
 
 $osName = "$((Select-WMI-Objects Win32_OperatingSystem | Select -First 1).Caption)"
 Write-Host "OS Name: $osName"
-Say "OS Name: $osName"
+Say "OS Name: [$osName], Build [$osBuild]"
 
 
 $ENV:PS1_TROUBLE_SHOOT = "On"
@@ -15,7 +15,7 @@ $ENV:SQLSERVERS_MEDIA_FOLDER = "C:\SQL-SETUP\Media"
 $ENV:SQLSERVERS_SETUP_FOLDER = "C:\SQL-SETUP\Installer"
 $ENV:PS1_REPO_DOWNLOAD_FOLDER = "C:\Temp"
 
-Download-File-FailFree-and-Cached "C:\ServiceMonitor.exe" "https://github.com/microsoft/IIS.ServiceMonitor/releases/download/v2.0.1.10/ServiceMonitor.exe"
+$isOk = Download-File-FailFree-and-Cached "C:\ServiceMonitor.exe" "https://github.com/microsoft/IIS.ServiceMonitor/releases/download/v2.0.1.10/ServiceMonitor.exe"
 
 Say "Installing IIS"
 Measure-Action "Installing IIS" { Add-WindowsFeature Web-Server }
