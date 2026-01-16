3325f96b629deef49bfe0c63189681db58dd55f3
devizer <devizer@github.io>
2026-01-16 23:58:01 +0200

    IIS Events, Build Web-Asp-Net, Web-Asp-Net45, Test-Windows-Docker-Images.yml [skip ci]

--- Windows-Docker-Images/Setup-Container.ps1
+++ Windows-Docker-Images/Setup-Container.ps1
@@ -6,14 +6,15 @@ $scriptDirectory = Split-Path -Parent -Path $MyInvocation.MyCommand.Path
 Write-Host "The current Setup-Container.ps1 script's directory is: [$scriptDirectory]"
 
 echo "Setup SqlServer-Version-Management ..."
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+[System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true};
 $urlSource = 'https://devizer.github.io/SqlServer-Version-Management/Install-SqlServer-Version-Management.ps1'; 
 foreach($attempt in 1..3) { try { iex ((New-Object System.Net.WebClient).DownloadString($urlSource)); break; } catch {Write-Host "Error downloading $urlSource"; sleep 0.1;} }
 
 # echo "Import-Module SqlServer-Version-Management ..."
 # Import-Module SqlServer-Version-Management
 
-$osName = "$((Select-WMI-Objects Win32_OperatingSystem | Select -First 1).Caption)"
+# $osName = "$((Select-WMI-Objects Win32_OperatingSystem | Select -First 1).Caption)" - 2019 Known Issue
 # NT 4.0+
 $osName = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ProductName
 Write-Host "OS Name: $osName"
@@ -21,18 +22,20 @@ Say "OS Name: [$osName], Build [$osBuild]"
 
 
 $ENV:PS1_TROUBLE_SHOOT = "On"
+& setx PS1_TROUBLE_SHOOT On
 $ENV:SQLSERVERS_MEDIA_FOLDER = "C:\SQL-SETUP\Media"
 $ENV:SQLSERVERS_SETUP_FOLDER = "C:\SQL-SETUP\Installer"
 $ENV:PS1_REPO_DOWNLOAD_FOLDER = "C:\Temp"
+setx PS1_REPO_DOWNLOAD_FOLDER C:\Temp
 
 Say "Setup Log Monitor and Service Monitor"
-$isOkServiceMonitor = Download-File-FailFree-and-Cached "C:\ServiceMonitor.exe" "https://github.com/microsoft/IIS.ServiceMonitor/releases/download/v2.0.1.10/ServiceMonitor.exe"
+$isOkServiceMonitor = Download-File-FailFree-and-Cached "C:\Container-Monitor\ServiceMonitor.exe" "https://github.com/microsoft/IIS.ServiceMonitor/releases/download/v2.0.1.10/ServiceMonitor.exe"
 Write-Host "Service Monitor Download Success: [$isOkServiceMonitor]"
 
 # https://github.com/microsoft/windows-container-tools/blob/main/LogMonitor/README.md
-$isOkLogMonitor = Download-File-FailFree-and-Cached "C:\LogMonitor\LogMonitor.exe" "https://github.com/microsoft/windows-container-tools/releases/download/v2.1.3/LogMonitor.exe"
+$isOkLogMonitor = Download-File-FailFree-and-Cached "C:\Container-Monitor\LogMonitor.exe" "https://github.com/microsoft/windows-container-tools/releases/download/v2.1.3/LogMonitor.exe"
 Write-Host "Log Monitor Download Success: [$isOkLogMonitor]"
-Copy-Item -Path "$scriptDirectory\LogMonitorConfig.json" -Destination "C:\LogMonitor\" -Force
+Copy-Item -Path "$scriptDirectory\LogMonitorConfig.json" -Destination "C:\Container-Monitor\" -Force
 
 
 Say "Installing .NET 3.5"
@@ -40,7 +43,7 @@ Say "Installing .NET 3.5"
 
 Say "Installing IIS"
 Measure-Action "Installing IIS" { 
-  # TODO: Web-Http-Redirect, Web-Basic-Auth, Web-Windows-Auth, Web-ASP, Web-Includes, Web-Http-Redirect
+  # TODO: Web-Http-Redirect, Web-Basic-Auth, Web-Windows-Auth, Web-ASP, Web-Includes
   $res = Add-WindowsFeature Web-Server, Web-Asp-Net, Web-Asp-Net45, Web-Scripting-Tools
   $res | ft -autosize | Out-String -width 1234
   if (-not $res.Success) { throw "Error Installing IIS. See Error Above" }
@@ -51,18 +54,14 @@ Say "Assign [v4.0] version for [DefaultAppPool]"
 # the same
 # Import-Module WebAdministration; Set-ItemProperty "IIS:\AppPools\DefaultAppPool" -Name managedRuntimeVersion -Value "v4.0"
 
-
-
+Write-Host " "
 Say "FINAL FEATURES"
 Get-WindowsFeature | ft -autosize | Out-String -Width 1234
 
-Write-Host ""
+Write-Host " "
 Say "Final NET Frameworks"
 . "$scriptDirectory\LIST-NET-Frameworks.ps1"
 
 Remove-Item -Path "C:\Temp" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "$ENV:TEMP" -Recurse -Force -ErrorAction SilentlyContinue
 New-Item -Path "$ENV:TEMP" -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
-
-
-
