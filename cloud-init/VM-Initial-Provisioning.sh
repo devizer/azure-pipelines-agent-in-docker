9e1f5067080c6985fb4ed420e4767291ff3c6b51
devizer <devizer@github.io>
2026-01-27 19:52:30 +0200

    3rd VM-Initial-Provisioning.sh, fix bzip2 and libc6-dev [skip ci]

--- cloud-init/VM-Initial-Provisioning.sh
+++ cloud-init/VM-Initial-Provisioning.sh
@@ -93,7 +93,7 @@ function Disbale-Unattended() {
 Disbale-Unattended
 
 # open-vm-tools?
-export APT_PACKAGES="debconf-utils jq gawk git sshpass sshfs rsync gcc make"
+export APT_PACKAGES="debconf-utils jq gawk git sshpass sshfs rsync gcc make libc6-dev"
 Say "Invloke apt-get install [$APT_PACKAGES]"
 # --force-yes is deprecated, but works on Debian 13 and Ubuntu 24.04
 (time (apt-get install -y --force-yes $APT_PACKAGES | { grep -v "(Reading database" || true; } || { for pack in $APT_PACKAGES; do Say "Installing one-by-one: $pack"; apt-get install -y -q $pack | { grep -v "(Reading database" || true; }; done; } )) |& tee /root/_logs/apt.install.txt # missing on old distros
