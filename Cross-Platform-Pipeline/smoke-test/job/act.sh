8c57b030bd023a9a7ebbe256081a9abdeba079b6
devizer <devizer@github.io>
2026-01-29 16:59:34 +0200

    2nd Update Build Crossplatform Pipeline.yml [skip ci]

--- Cross-Platform-Pipeline/smoke-test/job/act.sh
+++ Cross-Platform-Pipeline/smoke-test/job/act.sh
@@ -1,7 +1,9 @@
 set -eu; set -o pipefail
 Say "Perform smoke test job"
 (
-  echo "HOST: $(hostname)"; echo "KERNEL: $(uname -r)"; 
+  echo "HOST: $(hostname)"; 
+  echo "KERNEL: $(uname -r)"; 
+  echo "GLIBC: $(getconf GNU_LIBC_VERSION)"; 
   printf "\nMEMORY\n"; Drop-FS-Cache; free -m; 
   printf "\nSTORAGE\n"; df -h -T; 
   echo ""; url=https://raw.githubusercontent.com/devizer/glist/master/Benchmark-net40.exe; try-and-retry curl -ksfSL -o /tmp/Benchmark-net40.exe "$url"; time mono /tmp/Benchmark-net40.exe; 
@@ -10,8 +12,10 @@ Say "Perform smoke test job"
 
 echo $(uname -r) > kernel.txt
 getconf GNU_LIBC_VERSION > GLIBC-Version.txt
+hostname > hostname.txt
 
 rm -rf /var/lib/apt/* /var/cache/apt/* || true
 printf "\nONLINE APT\n";
-time apt-get update | grep -v "Reading" 2>/dev/null | tee -a results.txt
+time apt-get update -q | grep --line-buffered -v "Reading" 2>/dev/null | tee -a results.txt
+
 
