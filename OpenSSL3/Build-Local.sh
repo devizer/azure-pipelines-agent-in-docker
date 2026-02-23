39287261987d7f17c285b5b329dd1f48b34edf01
devizer <devizer@users.github.io>
2026-02-23 02:27:22 +0200

    Open SSL3 [skip ci]

--- OpenSSL3/Build-Local.sh
+++ OpenSSL3/Build-Local.sh
@@ -44,7 +44,7 @@ Benchmark-OpenSSL()
     for var_name in handshake_rsa2048 handshake_ecdsa256 transfer_aes128_128b transfer_aes128_16k transfer_aes256_128b transfer_aes256_16k; do
       var="${!var_name}";
       var_formatted="$(Format-Thousand "$var")"
-      printf "%-16s" $var_formatted >> "$summary_file"
+      printf "%16s" $var_formatted >> "$summary_file"
     done
     echo "" >> "$summary_file"
     Say "BENCHMARK SUMMARY"
