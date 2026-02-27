3dc80dc7275a7a4a03804a17f90745286672bb1c
devizer <devizer@users.github.io>
2026-02-27 19:02:15 +0200

    DevOps: test libssl,  [skip ci]

--- OpenSSL3/STEP-Test-OpenSSL3-Test-in-Container.sh
+++ OpenSSL3/STEP-Test-OpenSSL3-Test-in-Container.sh
@@ -12,7 +12,7 @@ Say "FOLDER: $(pwd -P)";
 
 tests_folder_base="./OpenSSL-Tests/$(Get-NET-RID)"
 
-find find $tests_folder_base -maxdepth 1 -type d | while IFS= read -r net_ver; do
+find $tests_folder_base -maxdepth 1 -type d | while IFS= read -r net_ver; do
   Say "Testing .NET $net_ver on $(Get-Linux-OS-Architecture) $(Get-Linux-OS-ID), RID='$(Get-NET-RID)'"
   exe=$net_ver/Test-OpenSSL
   ls -la "$exe" || true
