1dbb7997dc54ef01c6b8c999831152fe5dfcbd30
devizer <devizer@users.github.io>
2026-02-28 04:30:22 +0200

    DevOps: test libssl,  [skip ci]

--- OpenSSL3/STEP-Test-OpenSSL3-Test-in-Container.sh
+++ OpenSSL3/STEP-Test-OpenSSL3-Test-in-Container.sh
@@ -19,6 +19,7 @@ export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 SYSTEM_OPENSSL_VERSION=$(openssl version | awk '{print $2}' || true)
 summary_report_file="$SYSTEM_ARTIFACTSDIRECTORY/SUMMARY.$(Get-NET-RID).TXT"
 find $tests_folder_base -maxdepth 1 -type d | sort -V | while IFS= read -r folder; do
+  echo " "
   net_ver="$(basename $folder)"
   if [[ ! $net_ver =~ ^[1-9] ]]; then continue; fi
   Say "Starting Testing .NET=[$net_ver] on arch=[$(Get-Linux-OS-Architecture)] OS=[$(Get-Linux-OS-ID)], RID='$(Get-NET-RID)' ..."
@@ -34,26 +35,29 @@ find $tests_folder_base -maxdepth 1 -type d | sort -V | while IFS= read -r folde
   exe=$folder/Test-OpenSSL
   test_title="NET=${net_ver} ARCH=$(Get-Linux-OS-Architecture) RID=$(Get-NET-RID) OSID=$(Get-Linux-OS-ID) $ARTIFACT_NAME"
   log_name="$(Get-Safe-File-Name "$test_title")"
+  LOG_FULL_NAME="$SYSTEM_ARTIFACTSDIRECTORY/$log_name.Deafult.OpenSSL.log"
   # DEBUG
   Say-Definition "exe is" "$exe"
   ls -la "$exe" || true
   echo "test_title = [$test_title], log_name = [$log_name]"
-  if [[ -n "$(command -v file)" ]]; then file "$exe" || true; fi
-  if [[ -n "$(command -v file)" && -f $folder/libhostfxr.so ]]; then file "$folder/libhostfxr.so" || true; fi
+  {
+     echo "$test_title" 
+     if [[ -n "$(command -v file)" ]]; then file "$exe" || true; fi
+     if [[ -n "$(command -v file)" && -f $folder/libhostfxr.so ]]; then file "$folder/libhostfxr.so" || true; fi
+  } 2>&1 | tee -a "$LOG_FULL_NAME"
   # End DEBUG
-  LOG_FULL_NAME="$SYSTEM_ARTIFACTSDIRECTORY/$log_name"
   Colorize Magenta "STARTING TEST WITH DEFAULT OPENSSL: $test_title ... "
   pushd "$(dirname "$exe")" >/dev/null
   status_title="OK"
-  if ! (echo "$test_title"; "./$(basename "$exe")") 2>&1 | tee -a "$LOG_FULL_NAME.Deafult.OpenSSL.log"; then
+  if ! "./$(basename "$exe")" 2>&1 | tee -a "$LOG_FULL_NAME"; then
       Say --Display-As=Error "FAIL: $log_name"
       status_title="FAIL"
   fi
   popd >/dev/null
   echo "$(printf "%4s" "$status_title"): $test_title" | tee -a $summary_report_file
   Set-Json-File-Property "$JSON_REPORT_FILE" "STATUS" "$status_title"
-  echo " "
 done
 
 find $SYSTEM_ARTIFACTSDIRECTORY -name 'REPORT.*.JSON' | sort -V | xargs jq -s '.' > $SYSTEM_ARTIFACTSDIRECTORY/SUMMARY.REPORT.JSON
-
+Say "FINISH. SUMMARY Report"
+cat $SYSTEM_ARTIFACTSDIRECTORY/SUMMARY.REPORT.JSON
