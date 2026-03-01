c37a59300abbe23533a3a5123f24b58eacf1dfcf
devizer <devizer@users.github.io>
2026-03-02 01:20:56 +0200

    experimental curl [skip ci]

--- OpenSSL3/Experimental/Build-Static-Curl-In-Container.sh
+++ OpenSSL3/Experimental/Build-Static-Curl-In-Container.sh
@@ -4,7 +4,7 @@ apk add wget clang lld libc-dev cmake openssl-dev openssl-libs-static zlib-dev z
 apk add nghttp2-dev nghttp2-static
 apk add libssh2-dev libssh2-static
 
-apk add libunistring-static libunistring-dev libidn2-static libidn2-dev libpsl-static libpsl-dev 
+# apk add libunistring-static libunistring-dev libidn2-static libidn2-dev libpsl-static libpsl-dev 
 
 echo '
 curl 8.18.0 (x86_64-pc-linux-gnu) libcurl/8.18.0 OpenSSL/3.6.0 zlib/1.3.1 brotli/1.2.0 zstd/1.5.7 c-ares/1.34.6 libidn2/2.3.8 libpsl/0.21.5 libssh2/1.11.1 nghttp2/1.68.0 nghttp3/1.14.0
@@ -46,10 +46,9 @@ cmake -S . -B build \
   -DUSE_BROTLI=ON \
   -DUSE_LIBIDN2=ON \
   -DUSE_LIBPSL=ON \
-  -DLIBUNISTRING_LIBRARY=/usr/lib/libunistring.a \
   -DCURL_USE_LIBPSL=ON \
   -DCMAKE_FIND_LIBRARY_SUFFIXES=".a" \
-  -DCMAKE_EXE_LINKER_FLAGS="-static -Wl,--start-group /usr/lib/libidn2.a /usr/lib/libunistring.a /usr/lib/libpsl.a -Wl,--end-group" \
+  -DCMAKE_EXE_LINKER_FLAGS="-static" \
   -DUSE_ZSTD=ON \
   -DHTTP_ONLY=OFF \
   -DCURL_DISABLE_FTP=OFF \
